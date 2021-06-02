b94d2337bbe167fff0265a2d45a2a1effffc8976
Leonardo Lima <leonardo.alfs@gmail.com>
2021-06-03 00:16:22 +0200

    Fixing channel and adding interface

---
+++ src/channel.mli
@@ -0,0 +1,43 @@
+(*******************************************************************)
+(*     This is part of Explanator2, it is distributed under the    *)
+(*     terms of the GNU Lesser General Public License version 3    *)
+(*           (see file LICENSE for more details)                   *)
+(*                                                                 *)
+(*  Copyright 2021:                                                *)
+(*  Leonardo Lima (UCPH)                                           *)
+(*******************************************************************)
+
+open Util
+open Expl
+open Interval
+
+type input_type =
+  | Event of event
+  | Noise of string
+
+type input_channel =
+  | Input of in_channel
+  | InputMock of input_type list
+
+type output_type =
+  | Explanation of (timestamp * timepoint) * expl
+  | Info of string
+
+type output_channel =
+  | Output of out_channel
+  | OutputDebug of timepoint * out_channel
+  | OutputMock of output_type list
+
+type channel =
+  | IC of input_channel
+  | OC of output_channel
+
+exception End_of_mock of output_channel
+
+val input_event: input_channel -> output_channel -> event * input_channel
+val output_event: output_channel -> string -> output_channel
+val output_debug: int -> output_channel -> (unit -> string) -> output_channel
+
+val channel_to_string: channel -> string
+val output_explanation: output_channel -> (timestamp * timepoint) * expl -> output_channel
+val output_interval: output_channel -> interval -> output_channel
