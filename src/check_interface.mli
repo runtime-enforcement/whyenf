0c91a38ea53b246eb7fd26e88884810351d0d82e
Leonardo Lima <leonardo.alfs@gmail.com>
2021-07-01 23:36:54 +0200

    Adding check interface

---
+++ src/check_interface.mli
@@ -0,0 +1,14 @@
+(*******************************************************************)
+(*     This is part of Explanator2, it is distributed under the    *)
+(*     terms of the GNU Lesser General Public License version 3    *)
+(*           (see file LICENSE for more details)                   *)
+(*                                                                 *)
+(*  Copyright 2021:                                                *)
+(*  Leonardo Lima (UCPH)                                           *)
+(*******************************************************************)
+
+open Mtl
+open Expl
+open Util
+
+val check_proof: (Util.SS.t * int) list -> formula -> expl -> bool
