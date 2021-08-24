b9b4f1577bff9ad4e14943b023a60061471da067
Leonardo Lima <leonardo.alfs@gmail.com>
2021-08-24 14:15:52 +0200

    Adding test script

---
+++ test/test_seed.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+SIZE=$1
+ER=$2
+MEASURE=$3
+SEED=$4
+PREFIX="TMP_${SIZE}_${ER}_${SEED}"
+
+./gen_fmla ${PREFIX} ${SIZE} 50 0 1 ${SEED} 16
+./gen_log ${PREFIX} 100 ${ER} 4 ${SEED} 16
+OUT=$(../explanator2.native -O ${MEASURE} -mode all -debug -fmla ${PREFIX}.mdl -log ${PREFIX}.log 2>1 | grep "Checker output")
+echo -n "${PREFIX} ... "
+if [[ "${OUT}" == "" ]]
+        then echo "PASSED"
+fi
+if [[ "${OUT}" != "" ]]
+        then echo "FAILED"
+fi
