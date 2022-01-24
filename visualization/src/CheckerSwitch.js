00c0efa0306432fd20b72754939208142a09034c
Leonardo Lima <leonardo.alfs@gmail.com>
2022-01-24 14:45:39 +0000

    Fixing layout and adding new components

---
+++ visualization/src/CheckerSwitch.js
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Switch from '@mui/material/Switch';
+import Box from '@mui/material/Box';
+
+export default function CheckerSwitch() {
+  return (
+    <Box
+      component="form"
+      sx={{ mt: 4 }}
+    >
+      <FormControlLabel control={<Switch />} label="Verified Checker" />
+    </Box>
+  );
+}
