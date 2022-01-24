052b2fb34f8358a0595d237d45d9f4b01ee8e764
Leonardo Lima <leonardo.alfs@gmail.com>
2022-01-24 18:49:41 +0000

    Changing colors and fixing buttons

--- visualization/src/BottomBar.js
+++ visualization/src/BottomBar.js
@@ -6,14 +6,33 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import TwitterIcon from '@mui/icons-material/Twitter';
 
 export default function BottomBar() {
   return (
     <Box sx={{ mt: 4, flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h8" component="div" sx={{ flexGrow: 1 }}>
-            All rights reserved.
+          <Typography align="center" variant="h8" component="div" sx={{ flexGrow: 1 }}>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <GitHubIcon />
+            </IconButton>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <TwitterIcon />
+            </IconButton>
           </Typography>
         </Toolbar>
       </AppBar>
--- visualization/src/NavBar.js
+++ visualization/src/NavBar.js
@@ -4,29 +4,25 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import HelpIcon from '@mui/icons-material/Help';
+import InfoIcon from '@mui/icons-material/Info';
 
 export default function NavBar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="fixed">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            explanator
+            eXpLaNaToR
           </Typography>
-          <Button color="inherit">about</Button>
-        </Toolbar>
-      </AppBar>
+          <Button color="inherit">
+            <HelpIcon color="inherit" sx={{ mr: 0.5 }} /> help
+          </Button>
+          <Button color="inherit">
+            <InfoIcon color="inherit" sx={{ mr: 0.5 }} /> about
+          </Button>
+    </Toolbar>
+    </AppBar>
     </Box>
   );
 }
