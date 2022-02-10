de53ea98dff0d52a00c5c23b154719c04cfe1fee
Leonardo Lima <leonardo.alfs@gmail.com>
2022-02-10 22:49:10 +0000

    Adding cell click functionality

--- visualization/src/TimeGrid.js
+++ visualization/src/TimeGrid.js
@@ -1,96 +1,104 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 import Box from '@mui/material/Box';
 import { DataGrid } from '@mui/x-data-grid';
-import { mockData } from './data';
-import { pickColumnItem } from './util';
+import Button from '@mui/material/Button';
 import SquareIcon from '@mui/icons-material/Square';
+import { pickColumnItem, squareColor, updateSquares } from './util';
 
-const fixedColumns = [
-  {
-    field: 'tp',
-    headerName: 'TP',
-    width: 50,
-    sortable: false,
-    align: 'center',
-    disableClickEventBubbling: true
-  },
-  { field: 'ts',
-    headerName: 'TS',
-    width: 50,
-    sortable: false,
-    align: 'center',
-    disableClickEventBubbling: true
-  },
-  {
-    field: "f0",
-    headerName: mockData.formula,
-    width: 50,
-    sortable: false,
-    renderHeader: () => pickColumnItem(0),
-    renderCell: () => <SquareIcon />,
-    headerAlign: 'center',
-    align: 'center',
-    disableClickEventBubbling: true
+let mockData = require('./data.json');
+console.log(mockData);
+
+function Square(props) {
+  return (
+    <Button onClick={props.onClick}>
+      <SquareIcon style={{ color: props.value }} />
+    </Button>
+  );
+}
+
+function initSquares(explanationsLength, columns) {
+  var squares = [];
+  for (let tp = 0; tp < explanationsLength; ++tp) {
+    squares[tp] = {};
+    for (let j = 0; j < columns.length; ++j) {
+      if (j === 0) squares[tp][columns[j]] = squareColor(mockData.explanations[tp].explanation.type);
+      else squares[tp][columns[j]] = "primary";
+    }
   }
-];
-
-const dynamicColumns = mockData.subformulas.map((f, i) =>
-  ({
-    field: "f".concat((i+1).toString()),
-    headerName: f,
-    width: 50,
-    sortable: false,
-    renderHeader: () => pickColumnItem(i+1),
-    renderCell: () => <SquareIcon />,
-    headerAlign: 'center',
-    align: 'center',
-    disableClickEventBubbling: true
-  }));
-
-const rows = mockData.explanations.map((p, i) =>
-  ({
-    id: i,
-    tp: p.tp,
-    ts: p.ts,
-    f0: "",
-    f1: "",
-    f2: "",
-    f3: ""
-  }));
+  return squares;
+}
 
 function TimeGrid () {
-  const [selectedCell, setSelectedCell] = useState({});
-  const [openDialog, setOpenDialog] = useState(false);
+  let sq = initSquares(mockData.explanations.length, mockData.columns);
 
-  function currentlySelected(params: GridCellParams) {
-    const value = params.colDef.field;
-    // const api: GridApi = params.api;
+  const [squares, setSquares] = React.useState(sq);
 
-    if (value === "tp" || value === "ts") return;
+  const fixedColumns = [
+    {
+      field: 'tp',
+      headerName: 'TP',
+      width: 50,
+      sortable: false,
+      align: 'center',
+      disableClickEventBubbling: true
+    },
+    { field: 'ts',
+      headerName: 'TS',
+      width: 50,
+      sortable: false,
+      align: 'center',
+      disableClickEventBubbling: true
+    },
+    {
+      field: mockData.columns[0],
+      headerName: mockData.columns[0],
+      width: 50,
+      sortable: false,
+      renderHeader: () => pickColumnItem(0),
+      renderCell: (params) => <Square value={squares[params.row.tp][params.colDef.field]}
+                                      onClick={() => handleClick(params.row.tp, params.colDef.field)} />,
+      headerAlign: 'center',
+      align: 'center',
+      disableClickEventBubbling: true
+    }
+  ];
 
-    console.log(params);
+  const dynamicColumns = mockData.columns.slice(1).map((f, i) =>
+    ({
+      field: f,
+      headerName: f,
+      width: 50,
+      sortable: false,
+      renderHeader: () => pickColumnItem(i+1),
+      renderCell: () => <Square value="primary" />,
+      headerAlign: 'center',
+      align: 'center',
+      disableClickEventBubbling: true
+    }));
 
-    // const fields = api
-    //       .getAllColumns()
-    //       .map((c) => c.field)
-    //       .filter((c) => c !== "__check__" && !!c);
-    // const thisRow: Record<string, GridCellValue> = {};
+  const rows = mockData.explanations.map((p, i) =>
+    ({
+      id: i,
+      tp: p.tp,
+      ts: p.ts,
+      f0: "",
+      f1: "",
+      f2: "",
+      f3: ""
+    }));
 
-    // fields.forEach((f) => {
-    //   thisRow[f] = params.getValue(params.id, f);
-    // });
+  const handleClick = (tp, formString) => {
+    let newSquares = squares.slice();
+    setSquares({ squares: updateSquares(mockData.explanations[tp].explanation, mockData.subformulas, newSquares) });
+  };
 
-    // const user = {};
-    // user["id"] = Number(thisRow["id"]);
-    // user["name"] = thisRow["name"]!.toString();
-    // user["surname"] = thisRow["surname"]!.toString();
-
-    // setSelectedUser(user);
-    setOpenDialog(true);
-  }
-
-  const handleClose = () => {
-    setOpenDialog(false);
+  const renderSquare = (tp, formString) => {
+    return (
+      <Square
+        value={squares[tp][formString]}
+        onClick={() => handleClick(tp, formString)}
+      />
+    );
   };
 
   return (
@@ -100,7 +108,6 @@ function TimeGrid () {
         columns={fixedColumns.concat(dynamicColumns)}
         pageSize={13}
         rowsPerPageOptions={[5]}
-        onCellClick={currentlySelected}
         density="compact"
         disableColumnMenu
       />
--- visualization/src/util.js
+++ visualization/src/util.js
@@ -8,11 +8,84 @@ import Filter7Icon from '@mui/icons-material/Filter7';
 import Filter8Icon from '@mui/icons-material/Filter8';
 import Filter9Icon from '@mui/icons-material/Filter9';
 import CropSquareIcon from '@mui/icons-material/CropSquare';
+import Button from '@mui/material/Button';
+import { red, lightGreen } from '@mui/material/colors';
 
-export function pickColumnItem(i) {
+// function convertToMTL(explanation) {
+//   switch (explanation.type) {
+//   case "STT":
+//     return { type: "TT" };
+//   case "SAtom":
+//     return { type: "P", atom: explanation.atom };
+//   case "SNeg":
+//     return { type: "Neg", formula: convertToMTL(explanation.explanation) };
+//   case "SDisjL":
+//     return { type: "Disj", lformula: convertToMTL(explanation.explanation) };
+//   case "SDisjR":
+//     return { type: "Disj", rformula: convertToMTL(explanation.explanation) };
+//   case "SConj":
+//     return { type: "Conj",
+//              lformula: convertToMTL(explanation.lexplanation),
+//              rformula: convertToMTL(explanation.rexplanation) };
+//   case "SPrev":
+//     return {type: "Prev", formula: convertToMTL(explanation.explanation)};
+//   case "SNext":
+//     return {type: "Next", formula: convertToMTL(explanation.explanation)};
+//   case "SSince":
+//     return { type: "Since",
+//              lformula: convertToMTL(explanation.explanation),
+//              rformula: convertToMTL(explanation.explanations['0explanation']) };
+//   case "SUntil":
+//     return { type: "Until",
+//              lformula: convertToMTL(explanation.explanation),
+//              rformula: convertToMTL(explanation.explanations['0explanation']) };
+//   case "VFF":
+//     return { type: "FF" };
+//   case "VAtom":
+//     return { type: "P", atom: explanation.atom };
+//   case "VNeg":
+//     return {type: "Neg", formula: convertToMTL(explanation.explanation)};
+//   case "VDisj":
+//     return { type: "Disj",
+//              lformula: convertToMTL(explanation.lexplanation),
+//              rformula: convertToMTL(explanation.rexplanation) };
+//   case "VConjL":
+//     return { type: "Conj", lformula: convertToMTL(explanation.explanation) };
+//   case "VConjR":
+//     return { type: "Conj", rformula: convertToMTL(explanation.explanation) };
+//   case "VPrev":
+//     return { type: "Prev", formula: convertToMTL(explanation.explanation) };
+//   case "VNext":
+//     return { type: "Next", formula: convertToMTL(explanation.explanation) };
+//   case "VSince":
+//     return { type: "Since",
+//              lformula: convertToMTL(explanation.explanation),
+//              rformula: convertToMTL(explanation.explanations['0explanation']) };
+//   case "VSinceInf":
+//     return { type: "Since",
+//              rformula: convertToMTL(explanation.explanations['0explanation']) };
+//   case "VUntil":
+//     return { type: "Until",
+//              lformula: convertToMTL(explanation.explanation),
+//              rformula: convertToMTL(explanation.explanations['0explanation']) };
+//   case "VUntilInf":
+//     return { type: "Until",
+//              rformula: convertToMTL(explanation.explanations['0explanation']) };
+//   }
+// }
+
+export function pickColumnItem(i, f) {
   switch (i) {
   case 0:
-    return <Filter1Icon />;
+    return (
+      <Button
+        onClick={() => {
+          alert();
+        }}
+      >
+        <Filter1Icon />
+      </Button>
+    );
   case 1:
     return <Filter2Icon />;
   case 2:
@@ -32,4 +105,127 @@ export function pickColumnItem(i) {
   default:
     return <CropSquareIcon />;
   }
+}
+
+export function squareColor(explanationType) {
+  return (explanationType.charAt(0) === 'S' ? lightGreen[500] : red[500]);
+}
+
+function match(explanation, f) {
+  switch (explanation.type) {
+  case "STT":
+    return f.type === "TT";
+  case "SAtom":
+    return f.atom === explanation.atom;
+  case "SNeg":
+    return (f.type === "Neg") ? match(explanation.explanation, f.formula) : false;
+  case "SDisjL":
+    return (f.type === "Disj") ? match(explanation.explanation, f.lformula) : false;
+  case "SDisjR":
+    return (f.type === "Disj") ? match(explanation.explanation, f.rformula) : false;
+  case "SConj":
+    return (f.type === "Conj") ? (match(explanation.lexplanation, f.lformula) && match(explanation.rexplanation, f.rformula)) : false;
+  case "SPrev":
+    return (f.type === "Prev") ? match(explanation.explanation, f.formula) : false;
+  case "SNext":
+    return (f.type === "Next") ? match(explanation.explanation, f.formula) : false;
+  case "SSince":
+    return (f.type === "Since") ? (match(explanation.explanation, f.lformula) && match(explanation.explanations['0explanation'], f.rformula)) : false;
+  case "SUntil":
+    return (f.type === "Until") ? (match(explanation.explanation, f.lformula) && match(explanation.explanations['0explanation'], f.rformula)) : false;
+  case "VFF":
+    return f.type === "FF";
+  case "VAtom":
+    return f.atom === explanation.atom;
+  case "VNeg":
+    return (f.type === "Neg") ? match(explanation.explanation, f.formula) : false;
+  case "VDisj":
+    return (f.type === "Disj") ? (match(explanation.lexplanation, f.lformula) && match(explanation.rexplanation, f.rformula)) : false;
+  case "VConjL":
+    return (f.type === "Conj") ? match(explanation.explanation, f.lformula) : false;
+  case "VConjR":
+    return (f.type === "Conj") ? match(explanation.explanation, f.rformula) : false;
+  case "VPrev":
+    return (f.type === "Prev") ? match(explanation.explanation, f.formula) : false;
+  case "VNext":
+    return (f.type === "Next") ? match(explanation.explanation, f.formula) : false;
+  case "VSince":
+    return (f.type === "Since") ? (match(explanation.explanation, f.lformula) && match(explanation.explanations['0explanation'], f.rformula)) : false;
+  case "VSinceInf":
+    return (f.type === "Since") ? match(explanation.explanations['0explanation'], f.rformula) : false;
+  case "VUntil":
+    return (f.type === "Until") ? (match(explanation.explanation, f.lformula) && match(explanation.explanations['0explanation'], f.rformula)) : false;
+  case "VUntilInf":
+    return (f.type === "Until") ? match(explanation.explanations['0explanation'], f.rformula) : false;
+  default:
+    return false;
+  }
+}
+
+function findMatch(explanation, subformulas) {
+  for (let i = 0; i < subformulas.length; ++i) {
+    if (match(explanation, subformulas[i].formula)) return i;
+  }
+}
+
+export function updateSquares(explanation, subformulas, squares) {
+  var cells = [];
+
+  switch (explanation.type) {
+  case "STT":
+  case "SAtom":
+  case "VFF":
+  case "VAtom":
+    cells.push({ tp: explanation.tp,
+                 col: findMatch(explanation, subformulas),
+                 color: squareColor(explanation.type) });
+    break;
+  case "SNeg":
+  case "SDisjL":
+  case "SDisjR":
+  case "SPrev":
+  case "SNext":
+  case "VNeg":
+  case "VConjL":
+  case "VConjR":
+  case "VPrev":
+  case "VNext":
+    cells.push({ tp: explanation.explanation.tp,
+                 col: findMatch(explanation.explanation, subformulas),
+                 color: squareColor(explanation.explanation.type) });
+    break;
+  case "SConj":
+  case "VDisj":
+    cells.push({ tp: explanation.lexplanation.tp,
+                 col: findMatch(explanation.lexplanation, subformulas),
+                 color: squareColor(explanation.lexplanation.type) });
+    cells.push({ tp: explanation.rexplanation.tp,
+                 col: findMatch(explanation.rexplanation, subformulas),
+                 color: squareColor(explanation.rexplanation.type) });
+    break;
+  case "SSince":
+  case "SUntil":
+  case "VSince":
+  case "VUntil":
+    cells.push({ tp: explanation.explanation.tp,
+                 col: findMatch(explanation.explanation, subformulas),
+                 color: squareColor(explanation.explanation.type) });
+    cells.push({ tp: explanation.explanations['0explanation'].tp,
+                 col: findMatch(explanation.explanations['0explanation'], subformulas),
+                 color: squareColor(explanation.explanations['0explanation'].type) });
+    break;
+  case "VSinceInf":
+  case "VUntilInf":
+    cells.push({ tp: explanation.explanations['0explanation'].tp,
+                 col: findMatch(explanation.explanations['0explanation'], subformulas),
+                 color: squareColor(explanation.explanations['0explanation'].type) });
+    break;
+  default:
+    break;
+  }
+  console.log(cells);
+  for (let i = 0; i < cells.length; ++i) {
+    squares[cells[i].tp][cells[i].col] = cells[i].color;
+  }
+  return squares;
 }
