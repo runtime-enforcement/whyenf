82be168033acb44cb80d70c124eb6514475787a7
Leonardo Lima <leonardo.alfs@gmail.com>
2022-03-02 09:32:33 +0000

    Automating set of tests

---
+++ test/run_tests.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# * gen_fmla
+#
+# PREFIX | SIZE | MAXR | TYPE | SCALE | SEED | APS
+#                  10     0       1            16
+#
+# * gen_log
+# PREFIX | TS_CNT | ER | DELTA | SEED | APS
+#           100            4            16
+#
+# * useful combinations
+# ER    -> 1, 10, 100
+# SIZE  -> 10, 100
+# DELTA -> 4, 100
+# SCALE -> increase (1, 10)
