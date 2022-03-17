1c9211cfd07e888eaa2ff2297b153065348db141
Leonardo Lima <leonardo.alfs@gmail.com>
2022-03-17 18:13:53 +0100

    Starting integration with js_of_ocaml

--- lib/monitor.ml
+++ lib/monitor.ml
@@ -1051,3 +1051,28 @@ let monitor in_ch out_ch mode out_mode check le f =
       } in
     (ctx_updated, in_ch) in
   loop s (ctx, in_ch)
+
+let monitor2 log c le f =
+  let minimuml ps = minsize_list (get_mins le ps) in
+  let rec loop f x = loop f (f x) in
+  let mf = minit f in
+  let mf_ap = relevant_ap mf in
+  let ctx = { tp = 0
+            ; mf = mf
+            ; events = []
+            } in
+  let () = preamble_json f in
+  let s (ctx, in_ch) =
+    let ((sap, ts), in_ch) = input_event in_ch out_ch in
+    let sap_filtered = filter_ap sap mf_ap in
+    let events_updated = (sap_filtered, ts)::ctx.events in
+    let (ps, mf_updated) = meval' ctx.tp ts sap_filtered ctx.mf le minimuml in
+    let checker_ps = if check then Some (check_ps events_updated f (Deque.to_list ps)) else None in
+    let () = output_ps out_ch mode out_mode ts ctx.tp [] f (Deque.to_list ps) checker_ps in
+    let ctx_updated =
+      { tp = ctx.tp+1
+      ; mf = mf_updated
+      ; events = events_updated
+      } in
+    (ctx_updated, in_ch) in
+  loop s (ctx, in_ch)
