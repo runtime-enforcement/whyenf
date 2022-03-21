5a359a23287a9390fd155691fa7ecfbfee9b7252
Leonardo Lima <leonardo.alfs@gmail.com>
2022-03-21 14:08:56 +0100

    Improving main (bin) module
    Moreover, hard coded a (quick) test there.
    This test should be somewhere else.

--- bin/explanator2.ml
+++ bin/explanator2.ml
@@ -18,7 +18,7 @@ open Lib.Monitor
 
 module Explanator2 = struct
 
-  exception EXIT
+  exception UNEXPECTED_EXIT
 
   let check_ref = ref false
   let debug_ref = ref false
@@ -29,12 +29,13 @@ module Explanator2 = struct
   let fmla_ref = ref None
   let log_ref = ref stdin
   let out_ref = ref stdout
-  let last_tp = ref 0
+  let vis_ref = ref false
+  let log_str_ref = ref ""
 
   let usage () =
     Format.eprintf
       "Usage: ./explanator2.exe [ARGUMENTS]
-       Example: ./explanator2.exe -check -O size -fmla ex.mtl -log ex.log -out_mode plain
+       Example: ./explanator2.exe -check -O size -fmla ex.mtl -log ex.log
        Arguments:
        \t -check   - execute verified checker
        \t -mode
@@ -42,7 +43,7 @@ module Explanator2 = struct
        \t\t sat    - output only satisfaction explanations
        \t\t viol   - output only violation explanations
        \t -O (measure)
-       \t\t size   - optimize proof size (default)
+       \t\t size   - optimize proof size
        \t\t high   - optimize highest time-point occuring in a proof (lower is better)
        \t\t pred   - optimize multiset cardinality of atomic predicates
        \t\t none   - give any proof
@@ -55,20 +56,21 @@ module Explanator2 = struct
        \t\t json   - JSON output (only useful for the visualization)
        \t\t debug  - plain verbose output (only useful for debugging)
        \t -out
-       \t\t <file> - output file where the explanation is printed to (default: stdout)\n%!";
-    raise EXIT
+       \t\t <file> - output file where the explanation is printed to (default: stdout)
+       \t -vis     - execute monitor wrapper (only useful when debugging visualization related functions)\n%!";
+    exit 0
 
   let mode_error () =
     Format.eprintf "mode should be either \"sat\", \"viol\" or \"all\" (without quotes)\n%!";
-    raise EXIT
+    raise UNEXPECTED_EXIT
 
   let out_mode_error () =
     Format.eprintf "out_mode should be either \"plain\", \"json\" or \"debug\" (without quotes)\n%!";
-    raise EXIT
+    raise UNEXPECTED_EXIT
 
   let measure_error () =
     Format.eprintf "measure should be either \"size\", \"high\", \"pred\", or \"none\" (without quotes)\n%!";
-    raise EXIT
+    raise UNEXPECTED_EXIT
 
   let process_args =
     let rec go = function
@@ -131,6 +133,14 @@ module Explanator2 = struct
       | ("-out" :: outfile :: args) ->
          out_ref := open_out outfile;
          go args
+      | ("-vis" :: fmla :: args) ->
+         (* Quick sanity check (visualization related) *)
+         vis_ref := true;
+         log_str_ref := "@0 b\n@3 a\n@7 a\n@11 a b\n@13 a\n@17 a";
+         check_ref := true;
+         measure_le_ref := Some(size_le);
+         fmla_ref := Some(Lib.Mtl_parser.formula Lib.Mtl_lexer.token (Lexing.from_string fmla));
+         go args
       | [] -> ()
       | _ -> usage () in
     go
@@ -138,17 +148,17 @@ module Explanator2 = struct
   let _ =
     try
       process_args (List.tl (Array.to_list Sys.argv));
-      match !fmla_ref with
-      | None -> ()
-      | Some(f) -> let measure_le =
-                     match !measure_le_ref with
-                     | None -> size_le
-                     | Some measure_le' -> measure_le' in
-                   let _ = monitor !log_ref !out_ref !mode_ref !out_mode_ref !check_ref measure_le f in ()
+      let measure_le = Option.get !measure_le_ref in
+      let formula = Option.get !fmla_ref in
+      if !vis_ref then
+        let (_, out) = monitor2 !log_str_ref !check_ref measure_le formula in
+        Printf.printf "%s" out
+      else
+        let _ = monitor !log_ref !out_ref !mode_ref !out_mode_ref !check_ref measure_le formula in ()
     with
     | End_of_file -> (if !out_mode_ref = PLAIN then
                         closing_stdout !out_ref);
                      close_out !out_ref; exit 0
-    | EXIT -> close_out !out_ref; exit 1
+    | UNEXPECTED_EXIT -> close_out !out_ref; exit 1
 
 end
