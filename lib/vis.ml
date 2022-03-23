a2da42adcef4c02789907f7468a1154480b8d656
Leonardo Lima <leonardo.alfs@gmail.com>
2022-03-23 20:09:19 +0100

    Rewriting function (f -> proof -> cell * cells)

--- lib/vis.ml
+++ lib/vis.ml
@@ -16,47 +16,58 @@ module List = Base.List
 type idx = int
 type cell = timepoint * idx * bool
 type table = (cell * (cell list)) list
-type state = table * int
 
-let st_tbl st = match st with
-  | (tbl, _) -> tbl
-
-let st_idx st = match st with
-  | (_, idx) -> idx
-
-let next_idx l = (max_list l) + 1
-
-let rec update_state st f p =
+let rec update_state tbl idx f p =
   match f, p with
   | TT, S (STT i) ->
-     let cur_idx = st_idx st in
-     let cell = (p_at p, cur_idx, true) in
-     let tbl = (cell, []) :: (st_tbl st) in
-     (tbl, cur_idx)
+     let cell = (p_at p, idx, true) in
+     (cell, []) :: tbl
   | P s1, S (SAtom (i, s2)) ->
-     let cur_idx = st_idx st in
-     let cell = (p_at p, cur_idx, true) in
-     let tbl = (cell, []) :: (st_tbl st) in
-     (tbl, cur_idx)
+     let cell = (p_at p, idx, true) in
+     (cell, []) :: tbl
   | Neg f, S (SNeg vp) ->
-     let cur_idx = st_idx st in
-     let st_0 = update_state (st_tbl st, cur_idx+1) f (V vp) in
-     let cell = (p_at p, cur_idx, true) in
-     let cells = [(v_at vp, cur_idx+1, false)] in
-     let tbl = (cell, cells) :: (st_tbl st) in
-     (tbl, cur_idx)
-  | Since (i, f1, f2), S (SSince (sp2, sp1s)) ->
-     let cur_idx = st_idx st in
+     let vp_idx = idx+1 in
+     let tbl' = update_state tbl vp_idx f (V vp) in
+     let cell = (p_at p, idx, true) in
+     let cells = [(v_at vp, vp_idx, false)] in
+     (cell, cells) :: tbl'
+  | Disj (f1, _), S (SDisjL sp) ->
+     let sp_idx = idx+1 in
+     let tbl' = update_state tbl sp_idx f1 (S sp) in
+     let cell = (p_at p, idx, true) in
+     let cells = [(s_at sp, sp_idx, true)] in
+     (cell, cells) :: tbl'
+  | Disj (_, f2), S (SDisjR sp) ->
+     let sp_idx = idx+2 in
+     let tbl' = update_state tbl sp_idx f2 (S sp) in
+     let cell = (p_at p, idx, true) in
+     let cells = [(s_at sp, sp_idx, true)] in
+     (cell, cells) :: tbl'
+  | Conj (f1, f2), S (SConj (sp1, sp2)) ->
+     let sp1_idx = idx+1 in
+     let sp2_idx = idx+2 in
+     let tbl' = update_state tbl sp1_idx f1 (S sp1) in
+     let tbl'' = update_state tbl' sp2_idx f2 (S sp2) in
+     let cell = (p_at p, idx, true) in
+     let cells = [(s_at sp1, sp1_idx, true); (s_at sp2, sp2_idx, true)] in
+     (cell, cells) :: tbl''
+  | Prev (_, f), S (SPrev sp) ->
+     let sp_idx = idx+1 in
+     let tbl' = update_state tbl sp_idx f (S sp) in
+     let cell = (p_at p, idx, true) in
+     let cells = [(s_at sp, sp_idx, true)] in
+     (cell, cells) :: tbl'
+  | Since (_, f1, f2), S (SSince (sp2, sp1s)) ->
+     let sp1_idx = idx+1 in
+     let sp2_idx = idx+2 in
      (* Recursive calls *)
-     let st_0 = List.fold sp1s ~init:(st_tbl st, cur_idx+1) ~f:(fun st' sp1 -> update_state st' f1 (S sp1)) in
-     let st_1 = update_state (st_tbl st_0, cur_idx+2) f2 (S sp2) in
+     let tbl' = List.fold sp1s ~init:tbl ~f:(fun t sp1 -> update_state t sp1_idx f1 (S sp1)) in
+     let tbl'' = update_state tbl' sp2_idx f2 (S sp2) in
      (* State update *)
-     let cell = (p_at p, cur_idx, true) in
-     let cells = (s_at sp2, cur_idx+2, true) ::
-                   (List.map sp1s ~f:(fun sp1 -> (s_at sp1, cur_idx+1, true))) in
-     let tbl = (cell, cells) :: (st_tbl st_1) in
-     (tbl, cur_idx)
-  | _ -> failwith ""
+     let cell = (p_at p, idx, true) in
+     let cells = (s_at sp2, sp2_idx, true) ::
+                   (List.map sp1s ~f:(fun sp1 -> (s_at sp1, sp1_idx, true))) in
+     (cell, cells) :: tbl''
 
 let cell_to_json (tp, col, b) cells =
   let ident = "    " in
@@ -81,8 +92,8 @@ let cell_to_json (tp, col, b) cells =
 let expl_to_json f p =
   (* (Printf.printf "f = %s\n" (formula_to_string f));
    * (Printf.printf "p = %s\n" (expl_to_string p)); *)
-  let st = update_state ([], 0) f p in
+  let tbl = update_state [] 0 f p in
   let ident = "    " in
   (Printf.sprintf "%s\"table\": [\n" ident) ^
-  (String.concat ",\n" (List.map (st_tbl st) ~f:(fun (cell, cells) -> cell_to_json cell cells))) ^
+  (String.concat ",\n" (List.map tbl ~f:(fun (cell, cells) -> cell_to_json cell cells))) ^
   (Printf.sprintf "]")
