001986571f37c3f879133aa9fa4ea442353f0c4d
Leonardo Lima <leonardo.alfs@gmail.com>
2022-04-13 17:52:15 +0200

    Using onBlur on forms to improve performance

--- vis/src/FormulaTextField.js
+++ vis/src/FormulaTextField.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 
 export default function FormulaTextField ({ formula, setFormula }) {
 
+  const [localFormula, setLocalFormula] = useState("(a SINCE b) SINCE (a SINCE b)");
+
   const handleChange = (event) => {
+    setLocalFormula(event.target.value);
+  };
+
+  const handleBlur = (event) => {
     setFormula(event.target.value);
   };
+
+  useEffect(() => {
+    setFormula(localFormula);
+  }, [formula]);
 
   return (
     <Box
@@ -22,8 +32,9 @@ export default function FormulaTextField ({ formula, setFormula }) {
           required
           id="outlined-required"
           label="Formula"
-          value={formula}
+          value={localFormula}
           onChange={handleChange}
+          onBlur={handleBlur}
         />
       </div>
     </Box>
