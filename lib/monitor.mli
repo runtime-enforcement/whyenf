4f21ad7e5399fb416e6480593f69ed6ac3f2bc35
Leonardo Lima <leonardo.alfs@gmail.com>
2022-04-14 15:57:44 +0200

    Adding cur state as a parameter to jsoo functions

--- lib/monitor.mli
+++ lib/monitor.mli
@@ -79,5 +79,7 @@ type state =
   ; tp_ts: (timepoint, timestamp) Hashtbl.t
   }
 
-val monitor: in_channel -> out_channel -> mode -> out_mode -> bool -> (expl -> expl -> bool) -> formula -> out_channel
-val monitor2: ?mf:mformula -> ?st:state -> string -> bool -> (expl -> expl -> bool) -> formula -> (mformula * state) * string
+val monitor: in_channel -> out_channel -> mode -> out_mode -> bool -> (expl -> expl -> bool) -> formula ->
+             out_channel
+val monitor2: (mformula * state) option -> string -> bool -> (expl -> expl -> bool) -> formula ->
+              (mformula * state) option * string
