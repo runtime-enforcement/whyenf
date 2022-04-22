45ca1933f98b29f9e9aa859447bc9e39f5d3c993
Leonardo Lima <leonardo.alfs@gmail.com>
2022-04-22 13:06:20 +0200

    Removing unused code

--- vis/src/util.js
+++ vis/src/util.js
@@ -1,13 +1,3 @@
-import Filter1Icon from '@mui/icons-material/Filter1';
-import Filter2Icon from '@mui/icons-material/Filter2';
-import Filter3Icon from '@mui/icons-material/Filter3';
-import Filter4Icon from '@mui/icons-material/Filter4';
-import Filter5Icon from '@mui/icons-material/Filter5';
-import Filter6Icon from '@mui/icons-material/Filter6';
-import Filter7Icon from '@mui/icons-material/Filter7';
-import Filter8Icon from '@mui/icons-material/Filter8';
-import Filter9Icon from '@mui/icons-material/Filter9';
-import CropSquareIcon from '@mui/icons-material/CropSquare';
 import Button from '@mui/material/Button';
 import { red, purple, yellow, lightGreen } from '@mui/material/colors';
 
@@ -24,39 +14,6 @@ function computeMaxCol(explanations) {
   return maxCol;
 }
 
-export function pickColumnItem(i, f) {
-  switch (i) {
-  case 0:
-    return (
-      <Button
-        onClick={() => {
-          alert();
-        }}
-      >
-        <Filter1Icon />
-      </Button>
-    );
-  case 1:
-    return <Filter2Icon />;
-  case 2:
-    return <Filter3Icon />;
-  case 3:
-    return <Filter4Icon />;
-  case 4:
-    return <Filter5Icon />;
-  case 5:
-    return <Filter6Icon />;
-  case 6:
-    return <Filter7Icon />;
-  case 7:
-    return <Filter8Icon />;
-  case 8:
-    return <Filter9Icon />;
-  default:
-    return <CropSquareIcon />;
-  }
-}
-
 export function squareColor(bool) {
   return (bool ? lightGreen[500] : red[500]);
 }
@@ -68,15 +25,7 @@ export function squareColorTest(bool) {
 export function computeSquares(explanations, atoms, squares = []) {
 
   let maxRow = atoms.length;
-  let maxCol = 0;
-
-  // Find maxCol
-  for (let tp = 0; tp < explanations.length; ++tp) {
-    let tbl = explanations[tp].table;
-    for (let j = 0; j < tbl.length; ++j) {
-      if (tbl[j].col > maxCol) maxCol = tbl[j].col;
-    }
-  }
+  let maxCol = computeMaxCol(explanations);
 
   // Initialize empty squares
   squares = (squares.length === 0) ? new Array(maxRow).fill(null).map(() => Array(maxCol+3).fill("")) : squares;
