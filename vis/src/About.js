efd3abd8a445998d94f0cd4904c747a579ff1bf1
Leonardo Lima <leonardo.alfs@gmail.com>
2022-06-03 18:05:49 +0200

    Starting with About/Help pages

--- vis/src/About.js
+++ vis/src/About.js
@@ -1,50 +1,30 @@
-import React from 'react';
-import Grid from '@mui/material/Grid';
+import React, { useEffect, useState } from "react";
+import ReactMarkdown from 'react-markdown';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import Link from '@mui/material/Link';
-import NNFLogo from './assets/nnf_logo.png';
-import KULogo from './assets/ku_logo.png';
 
-const card = (
-  <React.Fragment>
-    <CardContent>
-      <Typography variant="body2">
-        {/*The <strong>Explanator2</strong> is an online monitoring tool that produces explanations for verdicts of
-        Metric Temporal Logic formulas on arbitrary traces. <br />
-        It is the successor of the <Link href="https://bitbucket.org/traytel/explanator/src/master/">Explanator</Link>.*/}
-        Soon.
-      </Typography>
-    </CardContent>
-    <Box sx={{ mt: 2, mb: 4 }}>
-      <Grid container
-            justifyContent="center"
-            alignItems="center"
-            spacing={0}>
-        <Grid item xs={9} sm={9} md={3} lg={3} xl={3}>
-          <Box component="img"
-               sx={{
-                 maxWidth: 276,
-                 maxHeight: 102,
-               }}
-               alt="University of Copenhagen's logo."
-               src={KULogo}>
-          </Box>
-        </Grid>
-      </Grid>
-    </Box>
-  </React.Fragment>
-);
+export default function About() {
+  const [content, setContent] = useState("");
 
-export default function Help() {
+  useEffect(() => {
+    fetch("About.md")
+      .then((res) => res.text())
+      .then((text) => setContent(text));
+  }, []);
+
   return (
-    <Box style={{ height: '90vh', margin: 0, padding: 0 }}>
+    <Box style={{ height: '100vh', margin: 0, padding: 0 }}>
       <Container maxWidth={false}>
         <Box sx={{ mt: 11 }}>
-          <Card variant="outlined">{card}</Card>
+          <Card variant="outlined">
+            <CardContent sx={{ mt: -2 }}>
+              <ReactMarkdown children={content} />
+            </CardContent>
+          </Card>
         </Box>
       </Container>
     </Box>
