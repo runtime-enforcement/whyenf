1db8e25bf8dd1da5d5cab9c551d0a9cecaf2c599
Leonardo Lima <leonardo.alfs@gmail.com>
2022-06-06 16:28:20 +0200

    Rewriting preamble

--- vis/src/components/PreambleCard.js
+++ vis/src/components/PreambleCard.js
@@ -3,15 +3,18 @@ import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
-import Link from '@mui/material/Link';
+import { Link } from 'react-router-dom';
 
 const card = (
   <React.Fragment>
     <CardContent>
+      <Typography variant="h6">
+        Welcome to the Explanator2,
+      </Typography>
       <Typography variant="body2">
-        This is a tool for online monitoring satisfaction and violation explanations
-        of MTL (Metric Temporal Logic) formulas on arbitrary traces. <br />
-        It is the successor of the <Link href="https://bitbucket.org/traytel/explanator/src/master/">Explanator</Link>.
+        This is a tool for online monitoring satisfaction and violation verdicts
+        of Metric Temporal Logic formulas on arbitrary traces. <br />
+        For more details, click <Link to="/about" style={{ textDecoration: 'none' }}>here</Link>.
       </Typography>
     </CardContent>
   </React.Fragment>
