8eccd0964f3b29b430b6ce62186276da4f094f10
Leonardo Lima <leonardo.alfs@gmail.com>
2022-11-24 13:59:16 +0100

    Rewriting example

--- formalization/mfotl/thys/Monitor.thy
+++ formalization/mfotl/thys/Monitor.thy
@@ -12,11 +12,11 @@ lift_definition trivial_partition :: "'v \<Rightarrow> ('d, 'v) partition" is "\
 lift_definition point_partition :: "'d \<Rightarrow> 'v \<Rightarrow> 'v \<Rightarrow> ('d, 'v) partition" is "\<lambda>d v1 v2. [({d}, v1), (UNIV - {d}, v2)]"
   by (auto simp add: less_Suc_eq)
 
-term " let leaf0 = (Leaf (Inr (VAndL (VPred 2 ''publish'' [MFOTL.Var a, MFOTL.Var f]))));
-       evproof = \<lambda>i. VExists (trivial_partition (VPred i ''approve'' [MFOTL.Var m, MFOTL.Var f])) in
+term "let leaf = (Leaf (Inr (VAndL (VPred 2 ''publish'' [MFOTL.Var a, MFOTL.Var f])))) in
+      let evproof = \<lambda>i. VExists (trivial_partition (VPred i ''approve'' [MFOTL.Var m, MFOTL.Var f])) in
   Node a (point_partition ''Alice'' 
-  (Node f (point_partition ''160'' (Leaf (Inl (SAnd (SPred 2 ''publish'' [MFOTL.Var a, MFOTL.Var f]) 
-                                   (SNeg (VOnce 2 0 (map evproof [0,1,2])))))) leaf0))
-  leaf0) :: (string, string) expl"
+    (Node f (point_partition ''160'' (Leaf (Inl (SAnd (SPred 2 ''publish'' [MFOTL.Var a, MFOTL.Var f]) 
+            (SNeg (VOnce 2 0 (map evproof [0,1,2])))))) leaf))
+  leaf) :: (string, string) expl"
 
 end
