d0e82c5ebe1bfb8fee5d90a7554ad0dc50aac1f4
Leonardo Lima <leonardo.alfs@gmail.com>
2022-12-19 23:01:16 +0100

    Starting with Cand/Opt

--- formalization/mfotl/thys/Monitor.thy
+++ formalization/mfotl/thys/Monitor.thy
@@ -1,35 +1,22 @@
 (*<*)
 theory Monitor
-  imports Proof_System
+  imports Proof_System "HOL-Library.Simps_Case_Conv"
 begin
 (*>*)
 
 lift_definition part_hd :: "('d, 'a) part \<Rightarrow> 'a" is "snd \<circ> hd" .
 
-lemma part_hd_Vals[termination_simp]: "part_hd part \<in> Vals part"
-  apply transfer
-   apply auto
-  oops
+find_theorems name:sum_list
 
-lemma part_hd_SForall_estimation[termination_simp]: "size (part_hd part) < size (SForall part)"
-  oops
+lemma size_part_hd_estimation[termination_simp]: "size (part_hd part) < Suc (size_part (\<lambda>_. 0) size part)"
+  apply transfer
+  apply (simp add: size_list_conv_sum_list)
+  sorry
 
 lemma size_last_estimation[termination_simp]: "xs \<noteq> [] \<Longrightarrow> size (last xs) < size_list size xs"
   by (induct xs) auto
 
-(* fun s_at'' :: "'d sproof \<Rightarrow> nat" where
-  "s_at'' (STT i) = i"
-| "s_at'' (SForall part) = s_at'' (part_hd part)" *)
-
-(* function (sequential) s_at' :: "'d sproof \<Rightarrow> nat" where
-  "s_at' (STT i) = i"
-| "s_at' (SForall part) = s_at' (part_hd part)"
-  by pat_completeness auto
-termination
-  apply(relation "measure f")
-   apply simp_all *)
-
-function (sequential) s_at :: "'d sproof \<Rightarrow> nat" and 
+fun s_at :: "'d sproof \<Rightarrow> nat" and 
   v_at :: "'d vproof \<Rightarrow> nat" where
   "s_at (STT i) = i"
 | "s_at (SPred i _ _) = i"
@@ -82,28 +69,23 @@ function (sequential) s_at :: "'d sproof \<Rightarrow> nat" and
 | "v_at (VSinceInf i _ _) = i"
 | "v_at (VUntil i _ _) = i"
 | "v_at (VUntilInf i _ _) = i"
-  by pat_completeness auto
-termination
-  sorry
 
-context fixes \<sigma> :: "'a MFOTL.trace"
+context fixes \<sigma> :: "'d MFOTL.trace"
 
 begin
 
-primrec eval_trm_set :: "'a MFOTL.envset \<Rightarrow> 'a MFOTL.trm \<Rightarrow> 'a set" where
-  "eval_trm_set vs (MFOTL.Var x) = vs x"
-| "eval_trm_set vs (MFOTL.Const x) = {x}"
+term set_Cons
 
-value "{5::nat} \<times> listset[{1::nat, 2}, {2::nat, 3}]"
+value "{''r''} \<times> (listset [{0::nat}, {2,0}, {1,0}])"
 
-fun s_check :: "'a MFOTL.envset \<Rightarrow> 'a MFOTL.formula \<Rightarrow> 'a sproof \<Rightarrow> bool"
-  and v_check :: "'a MFOTL.envset \<Rightarrow> 'a MFOTL.formula \<Rightarrow> 'a vproof \<Rightarrow> bool" where
+fun s_check :: "'d MFOTL.envset \<Rightarrow> 'd MFOTL.formula \<Rightarrow> 'd sproof \<Rightarrow> bool"
+  and v_check :: "'d MFOTL.envset \<Rightarrow> 'd MFOTL.formula \<Rightarrow> 'd vproof \<Rightarrow> bool" where
   "s_check vs f p = (case (f, p) of
     (MFOTL.TT, STT i) \<Rightarrow> True
   | (MFOTL.Pred r ts, SPred i s ts') \<Rightarrow> 
-    (r = s \<and> ts = ts' \<and> {r} \<times> listset (map (eval_trm_set vs) ts) \<subseteq> \<Gamma> \<sigma> i)
+    (r = s \<and> ts = ts' \<and> {r} \<times> listset (MFOTL.eval_trms_set vs ts) \<subseteq> \<Gamma> \<sigma> i)
   | (MFOTL.Eq t1 t2, SEq i t1' t2') \<Rightarrow> 
-    (t1 = t1' \<and> t2 = t2' \<and> (eval_trm_set vs t1) = (eval_trm_set vs t2))
+    (t1 = t1' \<and> t2 = t2' \<and> (MFOTL.eval_trm_set vs t1) = (MFOTL.eval_trm_set vs t2))
   | (MFOTL.Neg \<phi>, SNeg vp) \<Rightarrow> v_check vs \<phi> vp
   | (MFOTL.Or \<phi> \<psi>, SOrL sp1) \<Rightarrow> s_check vs \<phi> sp1
   | (MFOTL.Or \<phi> \<psi>, SOrR sp2) \<Rightarrow> s_check vs \<psi> sp2
@@ -113,7 +95,7 @@ fun s_check :: "'a MFOTL.envset \<Rightarrow> 'a MFOTL.formula \<Rightarrow> 'a 
   | (MFOTL.Iff \<phi> \<psi>, SIffSS sp1 sp2) \<Rightarrow> s_check vs \<phi> sp1 \<and> s_check vs \<psi> sp2 \<and> s_at sp1 = s_at sp2
   | (MFOTL.Iff \<phi> \<psi>, SIffVV vp1 vp2) \<Rightarrow> v_check vs \<phi> vp1 \<and> v_check vs \<psi> vp2 \<and> v_at vp1 = v_at vp2
   | (MFOTL.Exists x \<phi>, SExists sp) \<Rightarrow> undefined
-  | (MFOTL.Forall x \<phi>, SForall part) \<Rightarrow> undefined
+  | (MFOTL.Forall x \<phi>, SForall sp_part) \<Rightarrow> \<forall>sp \<in> Vals sp_part. s_check undefined \<phi> sp
   | (MFOTL.Prev \<I> \<phi>, SPrev sp) \<Rightarrow>
     (let j = s_at sp; i = s_at (SPrev sp) in 
     i = j+1 \<and> mem (\<Delta> \<sigma> i) \<I> \<and> s_check vs \<phi> sp)
@@ -153,9 +135,9 @@ fun s_check :: "'a MFOTL.envset \<Rightarrow> 'a MFOTL.formula \<Rightarrow> 'a 
 | "v_check vs f p = (case (f, p) of
     (MFOTL.FF, VFF i) \<Rightarrow> True
   | (MFOTL.Pred r ts, VPred i pred ts') \<Rightarrow> 
-    (r = pred \<and> ts = ts' \<and> {r} \<times> listset (map (eval_trm_set vs) ts) \<subseteq> - \<Gamma> \<sigma> i)
+    (r = pred \<and> ts = ts' \<and> {r} \<times> listset (map (MFOTL.eval_trm_set vs) ts) \<subseteq> - \<Gamma> \<sigma> i)
   | (MFOTL.Eq t1 t2, VEq i t1' t2') \<Rightarrow> 
-    (t1 = t1' \<and> t2 = t2' \<and> (eval_trm_set vs t1) \<noteq> (eval_trm_set vs t2))
+    (t1 = t1' \<and> t2 = t2' \<and> (MFOTL.eval_trm_set vs t1) \<noteq> (MFOTL.eval_trm_set vs t2))
   | (MFOTL.Neg \<phi>, VNeg sp) \<Rightarrow> s_check vs \<phi> sp
   | (MFOTL.Or \<phi> \<psi>, VOr vp1 vp2) \<Rightarrow> v_check vs \<phi> vp1 \<and> v_check vs \<psi> vp2 \<and> v_at vp1 = v_at vp2
   | (MFOTL.And \<phi> \<psi>, VAndL vp1) \<Rightarrow> v_check vs \<phi> vp1
@@ -169,7 +151,7 @@ fun s_check :: "'a MFOTL.envset \<Rightarrow> 'a MFOTL.formula \<Rightarrow> 'a 
     (let j = v_at vp; i = v_at (VPrev vp) in
     i = j+1 \<and> v_check vs \<phi> vp)
   | (MFOTL.Prev \<I> \<phi>, VPrevZ) \<Rightarrow>
-    v_at (VPrevZ::'a vproof) = 0
+    v_at (VPrevZ::'d vproof) = 0
   | (MFOTL.Prev \<I> \<phi>, VPrevOutL i) \<Rightarrow>
     i > 0 \<and> \<Delta> \<sigma> i < left \<I>
   | (MFOTL.Prev \<I> \<phi>, VPrevOutR i) \<Rightarrow>
@@ -221,6 +203,104 @@ fun s_check :: "'a MFOTL.envset \<Rightarrow> 'a MFOTL.formula \<Rightarrow> 'a 
     \<and> map v_at vp2s = [ETP_f \<sigma> i \<I> ..< hi + 1]
     \<and> (\<forall>vp2 \<in> set vp2s. v_check vs \<psi> vp2))
   | (_, _) \<Rightarrow> False)"
+
+declare s_check.simps[simp del] v_check.simps[simp del]
+simps_of_case s_check_simps[simp, code]: s_check.simps[unfolded prod.case] (splits: MFOTL.formula.split sproof.split)
+simps_of_case v_check_simps[simp, code]: v_check.simps[unfolded prod.case] (splits: MFOTL.formula.split vproof.split)
+
+lemma check_soundness:
+  "s_check vs \<phi> sp \<Longrightarrow> SAT \<sigma> v (s_at sp) \<phi>"
+  "v_check vs \<phi> vp \<Longrightarrow> VIO \<sigma> v (v_at vp) \<phi>"
+  oops
+
+lemma check_completeness:
+  assumes "MFOTL.future_bounded \<phi>"
+  shows "(SAT \<sigma> v i \<phi> \<longrightarrow> (\<exists>sp. s_at sp = i \<and> s_check vs \<phi> sp)) \<and>
+         (VIO \<sigma> v i \<phi> \<longrightarrow> (\<exists>vp. v_at vp = i \<and> v_check vs \<phi> vp))"
+  oops
+
+end
+
+section \<open>Algorithm\<close>
+
+definition proofApp :: "('d proof) \<Rightarrow> ('d proof) \<Rightarrow> ('d proof)" (infixl "\<oplus>" 65) where
+  "p \<oplus> q = (case (p, q) of
+   (Inl (SHistorically i li sps), Inl q) \<Rightarrow> Inl (SHistorically (i+1) li (sps @ [q]))
+ | (Inl (SAlways i hi sps), Inl q) \<Rightarrow> Inl (SAlways (i-1) hi (q # sps))
+ | (Inl (SSince sp2 sp1s), Inl q) \<Rightarrow> Inl (SSince sp2 (sp1s @ [q]))
+ | (Inl (SUntil sp1s sp2), Inl q) \<Rightarrow> Inl (SUntil (q # sp1s) sp2)
+ | (Inr (VSince i vp1 vp2s), Inr q) \<Rightarrow> Inr (VSince (i+1) vp1 (vp2s @ [q]))
+ | (Inr (VOnce i li vps), Inr q) \<Rightarrow> Inr (VOnce (i+1) li (vps @ [q]))
+ | (Inr (VEventually i hi vps), Inr q) \<Rightarrow> Inr (VEventually (i-1) hi (q # vps))
+ | (Inr (VSinceInf i li vp2s), Inr q) \<Rightarrow> Inr (VSinceInf (i+1) li (vp2s @ [q]))
+ | (Inr (VUntil i vp2s vp1), Inr q) \<Rightarrow> Inr (VUntil (i-1) (q # vp2s) vp1)
+ | (Inr (VUntilInf i hi vp2s), Inr q) \<Rightarrow> Inr (VUntilInf (i-1) hi (q # vp2s)))"
+
+definition proofIncr :: "('d proof) \<Rightarrow> ('d proof)" where
+  "proofIncr p = (case p of
+   Inl (SOnce i sp) \<Rightarrow> Inl (SOnce (i+1) sp)
+ | Inl (SEventually i sp) \<Rightarrow> Inl (SEventually (i-1) sp)
+ | Inl (SHistorically i li sps) \<Rightarrow> Inl (SHistorically (i+1) li sps)
+ | Inl (SAlways i hi sps) \<Rightarrow> Inl (SAlways (i-1) hi sps)
+ | Inr (VSince i vp1 vp2s) \<Rightarrow> Inr (VSince (i+1) vp1 vp2s)
+ | Inr (VOnce i li vps) \<Rightarrow> Inr (VOnce (i+1) li vps)
+ | Inr (VEventually i hi vps) \<Rightarrow> Inr (VEventually (i-1) hi vps)
+ | Inr (VHistorically i vp) \<Rightarrow> Inr (VHistorically (i+1) vp)
+ | Inr (VAlways i vp) \<Rightarrow> Inr (VAlways (i-1) vp)
+ | Inr (VSinceInf i li vp2s) \<Rightarrow> Inr (VSinceInf (i+1) li vp2s)
+ | Inr (VUntil i vp2s vp1) \<Rightarrow> Inr (VUntil (i-1) vp2s vp1)
+ | Inr (VUntilInf i hi vp2s) \<Rightarrow> Inr (VUntilInf (i-1) hi vp2s))"
+
+(* "min_list_wrt r xs = hd [x \<leftarrow> xs. \<forall>y \<in> set xs. r x y]" *)
+consts min_list_wrt :: "('d proof \<Rightarrow> 'd proof \<Rightarrow> bool) \<Rightarrow> 'd expl list \<Rightarrow> 'd expl"
+
+definition doOr :: "('d proof) \<Rightarrow> ('d proof) \<Rightarrow> ('d proof) list" where
+  "doOr p1 p2 = (case (p1, p2) of
+  (Inl sp1, Inl sp2) \<Rightarrow> [Inl (SOrL sp1), Inl (SOrR sp2)]
+| (Inl sp1, Inr _  ) \<Rightarrow> [Inl (SOrL sp1)]
+| (Inr _  , Inl sp2) \<Rightarrow> [Inl (SOrR sp2)]
+| (Inr vp1, Inr vp2) \<Rightarrow> [Inr (VOr vp1 vp2)])"
+
+definition doAnd :: "('d proof) \<Rightarrow> ('d proof) \<Rightarrow> ('d proof) list" where
+  "doAnd p1 p2 = (case (p1, p2) of
+  (Inl sp1, Inl sp2) \<Rightarrow> [Inl (SAnd sp1 sp2)]
+| (Inl _  , Inr vp2) \<Rightarrow> [Inr (VAndR vp2)]
+| (Inr vp1, Inl _  ) \<Rightarrow> [Inr (VAndL vp1)]
+| (Inr vp1, Inr vp2) \<Rightarrow> [Inr (VAndL vp1), Inr (VAndR vp2)])"
+
+definition doImp :: "('d proof) \<Rightarrow> ('d proof) \<Rightarrow> ('d proof) list" where
+  "doImp p1 p2 = (case (p1, p2) of
+  (Inl _  , Inl sp2) \<Rightarrow> [Inl (SImpR sp2)]
+| (Inl sp1, Inr vp2) \<Rightarrow> [Inr (VImp sp1 vp2)]
+| (Inr vp1, Inl sp2) \<Rightarrow> [Inl (SImpL vp1), Inl (SImpR sp2)]
+| (Inr vp1, Inr _  ) \<Rightarrow> [Inl (SImpL vp1)])"
+
+definition doIff :: "('d proof) \<Rightarrow> ('d proof) \<Rightarrow> ('d proof) list" where
+  "doIff p1 p2 = (case (p1, p2) of
+  (Inl sp1, Inl sp2) \<Rightarrow> [Inl (SIffSS sp1 sp2)]
+| (Inl sp1, Inr vp2) \<Rightarrow> [Inr (VIffSV sp1 vp2)]
+| (Inr vp1, Inl sp2) \<Rightarrow> [Inr (VIffVS vp1 sp2)]
+| (Inr vp1, Inr vp2) \<Rightarrow> [Inl (SIffVV vp1 vp2)])"
+
+locale alg = 
+  fixes \<sigma> :: "'d MFOTL.trace" and
+  wqo :: "('d proof) \<Rightarrow> ('d proof) \<Rightarrow> bool"
+begin
+
+function (sequential) Cand :: "MFOTL.name list \<Rightarrow> 'd MFOTL.envset \<Rightarrow> nat \<Rightarrow> 'd MFOTL.formula \<Rightarrow> ('d expl) list" and
+  Opt :: "MFOTL.name list \<Rightarrow> 'd MFOTL.envset \<Rightarrow> nat \<Rightarrow> 'd MFOTL.formula \<Rightarrow> 'd expl" where
+  "Cand vars vs i MFOTL.TT = [Leaf (Inl (STT i))]"
+| "Cand vars vs i MFOTL.FF = [Leaf (Inr (VFF i))]"
+| "Cand vars vs i (MFOTL.Pred r ts) = 
+  (case ({r} \<times> listset (MFOTL.eval_trms_set vs ts) \<subseteq> \<Gamma> \<sigma> i) of
+    True \<Rightarrow> [Leaf (Inl (SPred i r ts))] 
+  | False \<Rightarrow> [Leaf (Inr (VPred i r ts))])"
+| "Cand vars vs i (MFOTL.Or \<phi> \<psi>) = apply_pdt vars doOr (Opt vars vs i \<phi>) (Opt vars vs i \<psi>)"
+| "Cand vars vs i (MFOTL.And \<phi> \<psi>) = apply_pdt vars doAnd (Opt vars vs i \<phi>) (Opt vars vs i \<psi>)"
+| "Cand vars vs i (MFOTL.Imp \<phi> \<psi>) = apply_pdt vars doImp (Opt vars vs i \<phi>) (Opt vars vs i \<psi>)"
+| "Cand vars vs i (MFOTL.Iff \<phi> \<psi>) = apply_pdt vars doIff (Opt vars vs i \<phi>) (Opt vars vs i \<psi>)"
+| "Opt vars vs i \<phi> = min_list_wrt wqo (Cand vars vs i \<phi>)"
+  by pat_completeness auto
 
 end
 
