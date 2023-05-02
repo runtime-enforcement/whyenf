f65f6a5e6ab49ba505e10ccf851e874e8f8c1c6d
Leonardo Lima <leonardo.alfs@gmail.com>
2023-05-02 23:48:58 +0200

    Adding univ

--- src/mfotl/coset.mli
+++ src/mfotl/coset.mli
@@ -12,6 +12,7 @@ open Base
 
 type ('a, 'b) t = Finite of ('a, 'b) Set.t | Complement of ('a, 'b) Set.t
 
+val univ: ('a, 'b) Comparator.Module.t -> ('a, 'b) t
 val is_empty: ('a, 'b) t -> bool
 val inter: ('a, 'b) t -> ('a, 'b) t -> ('a, 'b) t
 val diff: ('a, 'b) t -> ('a, 'b) t -> ('a, 'b) t
