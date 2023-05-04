380c23dbce15b8cd83e53ec0997aa37907e7bc52
Leonardo Lima <leonardo.alfs@gmail.com>
2023-05-04 23:37:05 +0200

    Adding new module

---
+++ src/mfotl/import.ml
@@ -0,0 +1,11 @@
+(*******************************************************************)
+(*     This is part of Explanator2, it is distributed under the    *)
+(*     terms of the GNU Lesser General Public License version 3    *)
+(*           (see file LICENSE for more details)                   *)
+(*                                                                 *)
+(*  Copyright 2023:                                                *)
+(*  Leonardo Lima (UCPH)                                           *)
+(*******************************************************************)
+
+type timepoint = int
+type timestamp = int
