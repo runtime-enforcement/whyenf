7ecf73548ae6432b3893715d0299844f100a6244
Leonardo Lima <leonardo.alfs@gmail.com>
2023-05-08 17:22:10 +0000

    Fixing signature parsing in main module

---
+++ src/mfotl/sig_parser.mli
@@ -0,0 +1,11 @@
+(*******************************************************************)
+(*     This is part of Explanator2, it is distributed under the    *)
+(*     terms of the GNU Lesser General Public License version 3    *)
+(*           (see file LICENSE for parse_ntconst_rec details)      *)
+(*                                                                 *)
+(*  Copyright 2023:                                                *)
+(*  Dmitriy Traytel (UCPH)                                         *)
+(*  Leonardo Lima (UCPH)                                           *)
+(*******************************************************************)
+
+val parse: string -> unit
