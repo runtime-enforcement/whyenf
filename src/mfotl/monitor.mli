9cc9587c79e1707c242de83195878096c1dd85c3
Leonardo Lima <leonardo.alfs@gmail.com>
2023-05-11 17:26:34 +0200

    Starting exec function

---
+++ src/mfotl/monitor.mli
@@ -0,0 +1,10 @@
+(*******************************************************************)
+(*     This is part of Explanator2, it is distributed under the    *)
+(*     terms of the GNU Lesser General Public License version 3    *)
+(*           (see file LICENSE for more details)                   *)
+(*                                                                 *)
+(*  Copyright 2023:                                                *)
+(*  Leonardo Lima (UCPH)                                           *)
+(*******************************************************************)
+
+val exec: Io.Stdout.mode -> string -> Formula.t -> in_channel -> unit
