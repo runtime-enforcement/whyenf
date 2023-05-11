c7e6055bf4fe2024e251a149996ce22df9b8bb32
Leonardo Lima <leonardo.alfs@gmail.com>
2023-05-11 15:58:26 +0200

    Changing return type of trace parser

--- src/mfotl/other_parser.mli
+++ src/mfotl/other_parser.mli
@@ -1,15 +1,32 @@
 (*******************************************************************)
 (*     This is part of Explanator2, it is distributed under the    *)
 (*     terms of the GNU Lesser General Public License version 3    *)
-(*           (see file LICENSE for parse_ntconst_rec details)      *)
+(*           (see file LICENSE for more details)                   *)
 (*                                                                 *)
 (*  Copyright 2023:                                                *)
 (*  Dmitriy Traytel (UCPH)                                         *)
 (*  Leonardo Lima (UCPH)                                           *)
 (*******************************************************************)
 
+open Base
+
+module Parsebuf : sig
+
+  type t = { lexbuf: Lexing.lexbuf
+           ; mutable token: Other_lexer.token
+           ; mutable pred_sig: Pred.Sig.t option
+           ; mutable db: Db.t }
+
+end
+
 module Sig : sig
 
   val parse: string -> unit
+
+end
+
+module Trace : sig
+
+  val parse: in_channel -> Parsebuf.t
 
 end
