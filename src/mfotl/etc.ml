a59f5e62247aad6b3dd9a278cc271eff56804923
Leonardo Lima <leonardo.alfs@gmail.com>
2023-05-28 16:25:46 +0200

    Replacing local transpose with Base transpose

--- src/mfotl/etc.ml
+++ src/mfotl/etc.ml
@@ -49,10 +49,5 @@ let list_to_string indent f = function
      List.fold_left xs ~init:(indent ^ eat "[ " (f indent x))
        ~f:(fun s el -> eat (s ^ "\n" ^ indent ^ "; ") (f indent el)) ^ " ]"
 
-let rec transpose = function
-  | [] -> []
-  | [] :: xss -> transpose xss
-  | (x :: xs) :: xss -> (x :: List.map xss ~f:List.hd_exn) :: transpose (xs :: List.map xss ~f:List.tl_exn)
-
 let some_string () = (String.of_char (Random.ascii ())) ^ (String.of_char (Random.ascii ())) ^
                        (String.of_char (Random.ascii ())) ^ (String.of_char (Random.ascii ()))
