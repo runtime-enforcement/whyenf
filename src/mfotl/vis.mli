358d92a3d2f2fe46d4bbad41db76434ca04daaa5
Leonardo Lima <leonardo.alfs@gmail.com>
2023-05-30 19:56:50 +0000

    Adding sig file

---
+++ src/mfotl/vis.mli
@@ -0,0 +1,14 @@
+(*******************************************************************)
+(*     This is part of Explanator2, it is distributed under the    *)
+(*     terms of the GNU Lesser General Public License version 3    *)
+(*           (see file LICENSE for more details)                   *)
+(*                                                                 *)
+(*  Copyright 2023:                                                *)
+(*  Leonardo Lima (UCPH)                                           *)
+(*******************************************************************)
+
+module Expl : sig
+
+  val to_json: Formula.t -> Expl.t -> string
+
+end
