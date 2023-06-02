05d032d3f1d995c9f8bd3205f5a3a177ef229ce7
Leonardo Lima <leonardo.alfs@gmail.com>
2023-06-02 12:59:05 +0000

    Rewriting js_of_ocaml interface module

--- js/explanator2.ml
+++ js/explanator2.ml
@@ -8,58 +8,47 @@
 (*  Leonardo Lima (UCPH)                                           *)
 (*******************************************************************)
 
-open Util.Misc
-open Mtl.Expl
-open Mtl.Formula
-open Mtl.Io
-open Mtl.Parser
-open Mtl.Lexer
-open Mtl.Monitor
 open Js_of_ocaml
+open Mfotl
 
 module Explanator2 = struct
 
-  let validate_measure m =
-    match m with
-    | "size" -> size_le
-    | "wsize" -> high_le
-    | _ -> (fun _ _ -> true)
-
   let get_columns js_formula =
-    let formula = (Js_of_ocaml.Js.to_string js_formula) in
-    let f = Mtl.Parser.formula Mtl.Lexer.token (Lexing.from_string formula) in
-    Js.string (json_table_columns f)
+    let str_f = Js_of_ocaml.Js.to_string js_formula in
+    let f = Formula_parser.formula Formula_lexer.token (Lexing.from_string str_f) in
+    Js.string (Out.Json.table_columns f)
 
-  let monitor_init js_log js_measure js_formula =
-    let log = (Js_of_ocaml.Js.to_string js_log) in
-    let measure = validate_measure (Js_of_ocaml.Js.to_string js_measure) in
-    let formula = (Js_of_ocaml.Js.to_string js_formula) in
-    let formula_parsed = Mtl.Parser.formula Mtl.Lexer.token (Lexing.from_string formula) in
-    let (obj_opt, s) = monitor_vis None log measure formula_parsed in
-    (obj_opt, Js.string(s))
+  let monitor_init js_log js_sig js_formula =
+    let str_log = Js_of_ocaml.Js.to_string js_log in
+    let str_sig = Js_of_ocaml.Js.to_string js_sig in
+    Other_parser.Sig.parse_from_string str_sig;
+    let str_f = Js_of_ocaml.Js.to_string js_formula in
+    let f = Formula_parser.formula Formula_lexer.token (Lexing.from_string str_f) in
+    let (obj_opt, (es, dbs)) = Monitor.exec_vis None f str_log in
+    (obj_opt, (Js.string(es), Js.string(dbs)))
 
-  let monitor_append js_log js_measure js_formula obj_opt =
-    let log = (Js_of_ocaml.Js.to_string js_log) in
-    let measure = validate_measure (Js_of_ocaml.Js.to_string js_measure) in
-    let formula = (Js_of_ocaml.Js.to_string js_formula) in
-    let formula_parsed = Mtl.Parser.formula Mtl.Lexer.token (Lexing.from_string formula) in
-    let (obj_opt', s) = monitor_vis obj_opt log measure formula_parsed in
-    (obj_opt', Js.string(s))
+  let monitor_append js_log js_sig js_formula obj_opt =
+    let str_log = Js_of_ocaml.Js.to_string js_log in
+    let str_sig = Js_of_ocaml.Js.to_string js_sig in
+    Other_parser.Sig.parse_from_string str_sig;
+    let str_f = Js_of_ocaml.Js.to_string js_formula in
+    let f = Formula_parser.formula Formula_lexer.token (Lexing.from_string str_f) in
+    let (obj_opt', (es, dbs)) = Monitor.exec_vis obj_opt f str_log in
+    (obj_opt', (Js.string(es), Js.string(dbs)))
 
   let (_: unit) =
     Js.export_all
       (object%js
          method getColumns (js_formula: Js.js_string Js.t) = get_columns js_formula
          method monitorInit (js_log: Js.js_string Js.t)
-                  (js_measure: Js.js_string Js.t)
+                  (js_sig: Js.js_string Js.t)
                   (js_formula: Js.js_string Js.t) =
-           monitor_init js_log js_measure js_formula
+           monitor_init js_log js_sig js_formula
          method monitorAppend (js_log: Js.js_string Js.t)
-                  (js_measure: Js.js_string Js.t)
+                  (js_sig: Js.js_string Js.t)
                   (js_formula: Js.js_string Js.t)
-                  (obj_opt: (mformula * state) option)=
-           monitor_append js_log js_measure js_formula obj_opt
+                  (obj_opt: Monitor.MState.t option)=
+           monitor_append js_log js_sig js_formula obj_opt
        end)
-
 
 end
