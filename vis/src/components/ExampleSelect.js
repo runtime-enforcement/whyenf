0fb630c15eabb1bfcff4726306d220183c1216d5
Leonardo Lima <leonardo.alfs@gmail.com>
2023-06-03 23:31:23 +0000

    Updating examples

--- vis/src/components/ExampleSelect.js
+++ vis/src/components/ExampleSelect.js
@@ -6,25 +6,15 @@ import MenuItem from '@mui/material/MenuItem';
 import InputLabel from '@mui/material/InputLabel';
 
 const examples = [{
-  name: 'intrusion-detection',
-  formula: "NOT((out1 AND (PREV (ONCE in1) AND (EVENTUALLY[0,60] ids1))) OR (out2 AND (PREV (ONCE in2) AND (EVENTUALLY[0,60] ids2))) OR (out3 AND (PREV (ONCE in3) AND (EVENTUALLY[0,60] ids3))))",
-  trace: "@1 in1\n@2 in2 in3\n@50 out1\n@51 out2 out3\n@59 ids1\n@63 ids3"
+  name: 'publish-approve',
+  formula: "publish(r) IMPLIES ONCE[0,604800] approve(r)",
+  sig: "publish(x:int) approve(x:int)",
+  trace: "@1307532861 approve (152) @1307955600 approve (163) publish (160) @1308477599 approve (187) publish (163) (152)"
 }, {
-  name: 'recurbqr-past',
-  formula: "(r ∧ (¬q) ∧ (⧫ q)) → ((⧫[0,3] (p ∨ q)) S q)",
-  trace: "@0 q\n@1 p\n@2 r\n@3 q\n@4 \n@5 \n@6 p\n@7 \n@8 p\n@9 \n@10 \n@11 p\n@12 p\n@13 \n@14 p\n@15 p\n@16 r\n@17 q\n@18 p\n@19 \n@20 p\n@21 \n@22 \n@23 p\n@24 p\n@25 p\n@26 \n@27 p\n@28 \n@29 r\n@30 \n@31 q\n@32 \n@33 \n@34 p\n@35 p\n@36 p\n@37 \n@38 \n@39 p\n@40 p\n@41 r\n@42 q\n@43 \n@44 p\n@45 \n@46 \n@47 p\n@48 \n@49 \n@50 p\n@51 \n@52 \n@53 r\n@54 \n@55 \n@56 q\n@57 \n@58 \n@59 \n@60 \n@61 r"
-}, {
-  name: 'since-conjunction',
-  formula: "a S[1,2] (b ∧ c)",
-  trace: "@1 a b c\n@3 a b\n@3 a b \n@3 \n@3 a\n@4 a"
-}, {
-  name: 'respondbqr-past',
-  formula: "(r ∧ (¬q) ∧ (⧫ q)) → (((s → (⧫[0,3] p)) ∧ (¬((¬ s) S[3, ∞] p))) S q)",
-  trace: "@0 q\n@1 \n@2 p\n@3 \n@4 s\n@5 \n@6 p\n@7 s\n@8 \n@9 p\n@10 \n@11 \n@12 s\n@13 \n@14 p\n@15 \n@16 \n@17 s\n@18 \n@19 p\n@20 \n@21 \n@22 s\n@23 \n@24 p\n@25 s\n@26 \n@27 p\n@28 s\n@29 \n@30 r\n@31 q\n@32 \n@33 p\n@34 \n@35 \n@36 s\n@37 \n@38 p\n@39 s\n@40 \n@41 p\n@42 \n@43 s\n@44 \n@45 p\n@46 \n@47 \n@48 s\n@49 \n@50 p\n@51 s\n@52 \n@53 p\n@54 \n@55 s\n@56 \n@57 r\n@58 q\n@59 p\n@60 \n@61 \n@62 \n@63 \n@64 r"
-}, {
-  name: 'systemd-slices',
-  formula: "reachedTargetSliceUnits → (⧫[0,600] (createdSliceUser ∧ createdSliceSessionSlice)) ∧ ((⧫[600,∞] createdSliceGetty) ∧ (⧫[600,∞] createdSliceModprobe) ∧ (⧫[600,∞] createdSliceFsck))",
-  trace: "@3548334 \n@3578070 createdSliceGetty \n@3578293 createdSliceModprobe \n@3578449 createdSliceFsck \n@3578587 createdSliceUser createdSliceSessionSlice \n@3578635 \n@3578677 \n@3578762 \n@3578798 \n@3578824 \n@3578849 \n@3578871 \n@3578891 reachedTargetSliceUnits \n@3578913 \n@3578932"
+  name: 'publish-approve-manager',
+  formula: "publish(a,f) IMPLIES (ONCE[0,604800] (EXISTS m. (NOT mgr_F(m,a) SINCE mgr_S(m,a)) AND approve(m,f)))",
+  sig: "publish (a:string, f:int) approve (m:string, f:int) mgr_S (m:string, a:string) mgr_F (m:string, a:string)",
+  trace: "@1307532861 mgr_S (Mallory,Alice) mgr_S (Merlin,Bob) mgr_S (Merlin,Charlie) @1307532861 approve (Mallory,152) @1307955600 approve (Merlin,163) publish (Alice,160) mgr_F (Merlin,Charlie) @1308477599 approve (Merlin,187) publish (Bob,163) (Alice,163) (Charlie,163) (Charlie,152)"
 }];
 
 export default function ExampleSelect ({ setFormState }) {
@@ -68,10 +58,8 @@ export default function ExampleSelect ({ setFormState }) {
             onChange={handleChange}
             onClose={handleClose}
           >
-            <MenuItem value={"since-conjunction"}>Since (conjunction)</MenuItem>
-            <MenuItem value={"recurbqr-past"}>Bounded Recurrence</MenuItem>
-            <MenuItem value={"respondbqr-past"}>Bounded Response</MenuItem>
-            <MenuItem value={"systemd-slices"}>Systemd Slices</MenuItem>
+            <MenuItem value={"publish-approve"}>publish-approve</MenuItem>
+            <MenuItem value={"publish-approve-manager"}>publish-approve-manager</MenuItem>
           </Select>
         </FormControl>
       </div>
