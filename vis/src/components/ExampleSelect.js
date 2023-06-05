311ba35f13f7406de9b91e94b9f41b6573bd4adc
Leonardo Lima <leonardo.alfs@gmail.com>
2023-06-05 20:21:46 +0000

    Adding signature field to UI

--- vis/src/components/ExampleSelect.js
+++ vis/src/components/ExampleSelect.js
@@ -8,13 +8,13 @@ import InputLabel from '@mui/material/InputLabel';
 const examples = [{
   name: 'publish-approve',
   formula: "publish(r) IMPLIES ONCE[0,604800] approve(r)",
-  sig: "publish(x:int) approve(x:int)",
-  trace: "@1307532861 approve (152) @1307955600 approve (163) publish (160) @1308477599 approve (187) publish (163) (152)"
+  sig: "publish(x:int)\napprove(x:int)",
+  trace: "@1307532861 approve (152)\n@1307955600 approve (163) publish (160)\n@1308477599 approve (187) publish (163) (152)\n"
 }, {
   name: 'publish-approve-manager',
   formula: "publish(a,f) IMPLIES (ONCE[0,604800] (EXISTS m. (NOT mgr_F(m,a) SINCE mgr_S(m,a)) AND approve(m,f)))",
-  sig: "publish (a:string, f:int) approve (m:string, f:int) mgr_S (m:string, a:string) mgr_F (m:string, a:string)",
-  trace: "@1307532861 mgr_S (Mallory,Alice) mgr_S (Merlin,Bob) mgr_S (Merlin,Charlie) @1307532861 approve (Mallory,152) @1307955600 approve (Merlin,163) publish (Alice,160) mgr_F (Merlin,Charlie) @1308477599 approve (Merlin,187) publish (Bob,163) (Alice,163) (Charlie,163) (Charlie,152)"
+  sig: "publish (a:string, f:int)\napprove (m:string, f:int)\nmgr_S (m:string, a:string)\nmgr_F (m:string, a:string)",
+  trace: "@1307532861 mgr_S (Mallory, Alice) mgr_S (Merlin, Bob) mgr_S (Merlin, Charlie)\n@1307532861 approve (Mallory, 152)\n@1307955600 approve (Merlin, 163) publish (Alice, 160) mgr_F (Merlin, Charlie)\n@1308477599 approve (Merlin, 187) publish (Bob, 163) (Alice, 163) (Charlie, 163) (Charlie, 152)\n"
 }];
 
 export default function ExampleSelect ({ setFormState }) {
@@ -28,14 +28,14 @@ export default function ExampleSelect ({ setFormState }) {
   const handleClose = (event) => {
     const result = examples.find( ({ name }) => name === example );
     if (result !== undefined) {
-      setFormState({ type: 'setFormulaAndTrace', formula: result.formula, trace: result.trace });
+      setFormState({ type: 'setFormulaAndTraceAndSig', formula: result.formula, trace: result.trace, sig: result.sig });
     }
   };
 
   useEffect(() => {
     const result = examples.find( ({ name }) => name === example );
     if (result !== undefined) {
-      setFormState({ type: 'setFormulaAndTrace', formula: result.formula, trace: result.trace });
+      setFormState({ type: 'setFormulaAndTraceAndSig', formula: result.formula, trace: result.trace, sig: result.sig });
     }
   }, [example, setFormState, examples]);
 
