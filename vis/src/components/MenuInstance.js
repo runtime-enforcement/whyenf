deb8166e9d3240699aad1bf6800c4fc219047458
Leonardo Lima <leonardo.alfs@gmail.com>
2023-06-20 09:59:26 +0000

    Changing symbol for complement set

--- vis/src/components/MenuInstance.js
+++ vis/src/components/MenuInstance.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import ArrowRightIcon from '@mui/icons-material/ArrowRight';
+import SettingsEthernetIcon from '@mui/icons-material/SettingsEthernet';
+import DeselectIcon from '@mui/icons-material/Deselect';
 import MenuItem from '@mui/material/MenuItem';
 import { IconMenuItem, NestedMenuItem } from 'mui-nested-menu';
 
@@ -11,7 +13,7 @@ function MenuInstance ({ explObj, open, handleClose, handleClick }) {
         <MenuItem disabled={true}>{explObj.var}</MenuItem>
         { explObj?.part?.map((el, i) => {
           const ds = el.subset_type === "finite" ?
-                el.subset_values.join(', ') : '*';
+                el.subset_values.join(', ') : <SettingsEthernetIcon/>;
           if (el.type === "node") {
             return (
               <div key={i}>
