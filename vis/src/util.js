4312765108832212af76a858b0526d59d1a24ed9
Leonardo Lima <leonardo.alfs@gmail.com>
2023-06-20 16:31:37 +0200

    Fixing boolean checking

--- vis/src/util.js
+++ vis/src/util.js
@@ -81,75 +81,28 @@ export function initRhsTable(dbsObjs, subfsColumns) {
 
 }
 
-export function exposeColorsTable(explObj, maxRow, maxCol, explsTable = []) {
+export function exposeColorsTable(explObj, maxRow, maxCol, colorsTable = []) {
 
   // Initialize empty matrix
-  explsTable = (explsTable.length === 0) ? new Array(maxRow).fill(null).map(() => Array(maxCol).fill("")) : explsTable;
+  colorsTable = (colorsTable.length === 0) ? new Array(maxRow).fill(null).map(() => Array(maxCol).fill("")) : colorsTable;
 
   // Expose boolean verdict in main subformula column
   let tblIndex = explObj.table.findIndex(tbl => tbl.col === 0);
   let tbl = explObj.table[tblIndex];
-  explsTable[tbl.tp][tbl.col] = tbl.bool ? cellColor(true) : cellColor(false);
+  colorsTable[tbl.tp][tbl.col] = tbl.bool === "true" ? cellColor(true) : cellColor(false);
 
   // Expose (as a black cell) the boolean subproofs
   for (let i = 0; i < explObj.table.length; ++i) {
     let tbl = explObj.table[i];
     for (let j = 0; j < tbl.cells.length; ++j) {
-      if (explsTable[tbl.cells[j].tp][tbl.cells[j].col] === "") {
-        explsTable[tbl.cells[j].tp][tbl.cells[j].col] = black;
+      if (colorsTable[tbl.cells[j].tp][tbl.cells[j].col] === "") {
+        colorsTable[tbl.cells[j].tp][tbl.cells[j].col] = black;
       }
     }
   }
 
-  return explsTable;
+  return colorsTable;
 
-}
-
-export function computeSquares(dbsObjs, explsObjs, squares = []) {
-
-  // let maxRow = Math.max(explanations.length, atoms.length);
-  // let maxCol = computeMaxCol(explanations) + 1;
-  let maxRow = Math.max(dbsObjs.length);
-  let maxCol = dbsObjs.nCols;
-
-  // Initialize empty squares
-  squares = (squares.length === 0) ? new Array(maxRow).fill(null).map(() => Array(maxCol).fill("")) : squares;
-
-  // Populate cells with dbs
-  // for (let tp = 0; tp < dbsObjs.length; ++tp) {
-  //   if (dbsObjs[tp].dbs_row.length != 0) {
-  //     let dbs = dbsObjs[tp].dbs;
-  //     for (let j = 0; j < dbs[tp].length; ++j) {
-  //       if (tp === dbs[j].tp) squares[tp][dbs[j].col] = dbs[j].db;
-  //     }
-  //   } else {
-  //     for (let j = 0; j < maxCol; ++j) {
-  //       if (tp === dbs[j].tp) squares[tp][dbs[j].col] = dbs[j].db;
-  //     }
-  //   }
-  // }
-
-  // Populate main subformula column with data
-  // for (let tp = 0; tp < explanations.length; ++tp) {
-  //   let tbl = explanations[tp].table;
-  //   for (let j = 0; j < tbl.length; ++j) {
-  //     if (tp === tbl[j].tp && tbl[j].col === atoms[0].aps.length) {
-  //       squares[tp][tbl[j].col] = tbl[j].bool ? squareColor(true) : squareColor(false);
-  //     }
-  //   }
-  // }
-
-  // Initialize the other parts of the explanations with black
-  // for (let i = 0; i < explanations.length; ++i) {
-  //   for (let j = 0; j < explanations[i].table.length; ++j) {
-  //     let tbl = explanations[i].table[j];
-  //     for (let k = 0; k < tbl.cells.length; ++k) {
-  //       squares[tbl.cells[k].tp][tbl.cells[k].col] = black;
-  //     }
-  //   }
-  // }
-
-  return squares;
 }
 
 export function tpsIn(ts, tp, interval, period, lastTS, atoms) {
