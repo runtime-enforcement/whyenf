8360d4aac540991dd71cb70b678a0b90735729e9
Leonardo Lima <leonardo.alfs@gmail.com>
2023-06-27 09:46:41 +0000

    Adding data race example to visualization

--- vis/src/components/ExampleSelect.js
+++ vis/src/components/ExampleSelect.js
@@ -15,6 +15,11 @@ const examples = [{
   formula: "publish(a,f) → (⧫[0,604800] (∃ m. (¬ mgr_F(m,a) S mgr_S(m,a)) ∧ approve(m,f)))",
   sig: "publish (a:string, f:int)\napprove (m:string, f:int)\nmgr_S (m:string, a:string)\nmgr_F (m:string, a:string)",
   trace: "@1307532861 mgr_S (Mallory, Alice) mgr_S (Merlin, Bob) mgr_S (Merlin, Charlie)\n@1307532861 approve (Mallory, 152)\n@1307955600 approve (Merlin, 163) publish (Alice, 160) mgr_F (Merlin, Charlie)\n@1308477599 approve (Merlin, 187) publish (Bob, 163) (Alice, 163) (Charlie, 163) (Charlie, 152)\n"
+}, {
+  name: 'data-race',
+  formula: "((ONCE (read(t1,x) OR write(t1,x))) AND (ONCE write(t2,x))) IMPLIES EXISTS l. ((NOT ONCE NOT ((read(t1,x) OR write(t1,x)) IMPLIES (NOT rel(t1,l) SINCE acq(t1,l)))) AND (NOT ONCE NOT ((read(t2,x) OR write(t2,x)) IMPLIES (NOT rel(t2,l) SINCE acq(t2,l)))))",
+  sig: "acq(x:int,y:int)\nrel(x:int,y:int)\nread(x:int,y:int)\nwrite(x:int,y:int)\n",
+  trace: "@0 acq(9,9)\n@1 read(9,3)\n@2 acq(13,19)\n@3 acq(15,3)\n@4 acq(18,15)\n@5 read(13,5)\n@6 write(15,4)\n@7 write(15,3)\n@8 acq(17,13)\n@9 write(15,9)\n@10 write(13,13)\n@11 acq(8,11)\n@12 write(18,4)\n@13 rel(9,9)\n@14 acq(10,10)\n@15 read(15,4)\n@16 write(15,9)\n@17 write(13,10)\n@18 acq(7,6)\n@19 acq(0,5)\n"
 }];
 
 export default function ExampleSelect ({ setFormState }) {
@@ -60,6 +65,7 @@ export default function ExampleSelect ({ setFormState }) {
           >
             <MenuItem value={"publish-approve"}>publish-approve</MenuItem>
             <MenuItem value={"publish-approve-manager"}>publish-approve-manager</MenuItem>
+            <MenuItem value={"data-race"}>data-race</MenuItem>
           </Select>
         </FormControl>
       </div>
