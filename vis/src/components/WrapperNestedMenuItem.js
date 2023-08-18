4b3d15b7ebdf460262571a244676534151ab6535
Leonardo Lima <leonardo.alfs@proton.me>
2023-08-18 16:26:56 +0200

    Adding (check/x)mark to dropdown menu options

--- vis/src/components/WrapperNestedMenuItem.js
+++ vis/src/components/WrapperNestedMenuItem.js
@@ -1,7 +1,12 @@
 import React, { useState } from 'react';
 import { NestedMenuItem } from 'mui-nested-menu';
+import CancelIcon from '@mui/icons-material/Cancel';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import Icon from '@mui/material/Icon';
+import { red, lightGreen } from '@mui/material/colors';
+import { getPolarity } from '../util';
 
-function WrapperNestedMenuItem ({ label, rightIcon, parentMenuOpen, children }) {
+function WrapperNestedMenuItem ({ label, rightIcon, explObj, curCol, parentMenuOpen, children }) {
 
   const [menuSelected, setMenuSelected] = useState(false);
 
@@ -13,10 +18,29 @@ function WrapperNestedMenuItem ({ label, rightIcon, parentMenuOpen, children }) 
     setMenuSelected(false);
   };
 
+  const leftIcon =
+        (getPolarity(explObj, curCol) === "true") ?
+        <span>
+          <CheckCircleIcon fontSize="small" style={{ color: lightGreen[500] }}/>
+          <Icon fontSize="small"/>
+        </span>
+        : (getPolarity(explObj, curCol) === "false" ?
+           <span>
+             <Icon fontSize="small"/>
+             <CancelIcon fontSize="small" style={{ color: red[500] }}/>
+           </span>
+           : (getPolarity(explObj, curCol) === "both" ?
+              <span>
+                <CheckCircleIcon fontSize="small" style={{ color: lightGreen[500] }}/>
+                <CancelIcon fontSize="small" style={{ color: red[500] }}/>
+              </span> : ""));
+
   return (
     <div onMouseEnter={handleMouseEnter}
          onMouseLeave={handleMouseLeave}>
-      <NestedMenuItem rightIcon={rightIcon}
+
+      <NestedMenuItem leftIcon={leftIcon}
+                      rightIcon={rightIcon}
                       label={label}
                       parentMenuOpen={parentMenuOpen}
                       style={ menuSelected ? {background: "#fdd835"} : {}}>
