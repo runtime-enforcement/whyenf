fef0e0ba51e1530273583fba9e7a88be2e38b647
Leonardo Lima <leonardo.alfs@proton.me>
2023-08-18 17:11:20 +0200

    Adding (check/x)mark to dropdown menu (final) options

---
+++ vis/src/components/WrapperIconMenuItem.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import { IconMenuItem } from 'mui-nested-menu';
+import CancelIcon from '@mui/icons-material/Cancel';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import Icon from '@mui/material/Icon';
+import { red, lightGreen } from '@mui/material/colors';
+import { getPolarity } from '../util';
+
+function WrapperIconMenuItem ({ label, rightIcon, explObj, curCol, handleClick }) {
+
+  const [menuSelected, setMenuSelected] = useState(false);
+
+  const handleMouseEnter = () => {
+    setMenuSelected(true);
+  };
+
+  const handleMouseLeave = () => {
+    setMenuSelected(false);
+  };
+
+  const leftIcon =
+        (getPolarity(explObj, curCol) === "true") ?
+        <span>
+          <CheckCircleIcon fontSize="small" style={{ color: lightGreen[500] }}/>
+          <Icon fontSize="small"/>
+        </span>
+        : (getPolarity(explObj, curCol) === "false" ?
+           <span>
+             <Icon fontSize="small"/>
+             <CancelIcon fontSize="small" style={{ color: red[500] }}/>
+           </span>
+           : (getPolarity(explObj, curCol) === "both" ?
+              <span>
+                <CheckCircleIcon fontSize="small" style={{ color: lightGreen[500] }}/>
+                <CancelIcon fontSize="small" style={{ color: red[500] }}/>
+              </span> : ""));
+
+  return (
+    <div onMouseEnter={handleMouseEnter}
+         onMouseLeave={handleMouseLeave}>
+      <IconMenuItem leftIcon={leftIcon}
+                    rightIcon={rightIcon}
+                    label={label}
+                    style={ menuSelected ? {background: "#fdd835"} : {} }
+                    onClick={handleClick}>
+      </IconMenuItem>
+    </div>
+  );
+}
+
+export default WrapperIconMenuItem;
