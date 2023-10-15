4f71a919085609e83e3e103fd53dbce864665bfa
Leonardo Lima <leonardo.alfs@proton.me>
2023-10-15 16:44:20 +0200

    Rewriting MTL tests

--- test/test_seed.sh
+++ test/test_seed.sh
@@ -1,136 +1,52 @@
 #!/bin/bash
 
-# ./test_seed.sh no-check verb 10 1 1 4 size 144
+# ./test_seed.sh verified 10 1 1 4 144
 
 # Input parameters:
-MODE=$3
-SIZE=$4
-SCALE=$5
-ER=$6
-DELTA=$7
-MEASURE=$8
-OPERATORS=$9
-SEED=${10}
+SIZE=$2
+SCALE=$3
+ER=$4
+DELTA=$5
+SEED=$6
 
 # Flags:
 CHECK_FLAG=$1
-WEIGHT_FLAG=$2
 
 # Variables:
-PREFIX="TMP_${SIZE}_${SCALE}_${ER}_${DELTA}_${MEASURE}_${SEED}"
+PREFIX="TMP_${SIZE}_${SCALE}_${ER}_${DELTA}_${SEED}"
 OUT=""
 
 usage () {
-    printf "usage: test_seed.sh [check or no-check] [weight or no-weight] [mode] [size] [scale] [er] [delta] [measure] [mtl or extended-mtl] [seed]\n"
+    printf "usage: test_seed.sh [verified or unverified] [size] [scale] [er] [delta] [seed]\n"
     exit 1
 }
 
-simp () {
-    if [[ "${OPERATORS}" = "mtl" ]]
-    then
-        ./tmp/gen_fmla ${PREFIX} ${SIZE} 10 0 ${SCALE} ${SEED} 16
-    else
-        ./tmp/gen_fmla_full ${PREFIX} ${SIZE} 10 0 ${SCALE} ${SEED} 16
-    fi
-
-    ./tmp/gen_log ${PREFIX} 100 ${ER} ${DELTA} ${SEED} 16
-    mv ${PREFIX}.* tmp/
-
-    if [[ "${CHECK_FLAG}" = "check" ]]
-    then
-        if [[ "${WEIGHT_FLAG}" = "weight" ]]
-        then
-            OUT=$(../bin/explanator2.exe -O ${MEASURE} -mode all -fmla tmp/${PREFIX}.mdl -log tmp/${PREFIX}.log \
-                                         -check -out_mode plain -weights test_weights.ws 2>&1)
-        else
-            OUT=$(../bin/explanator2.exe -O ${MEASURE} -mode all -fmla tmp/${PREFIX}.mdl -log tmp/${PREFIX}.log -out_mode plain \
-                                         -check 2>&1)
-        fi
-    else
-        if [[ "${CHECK_FLAG}" = "no-check" ]]
-        then
-            if [[ "${WEIGHT_FLAG}" = "weight" ]]
-            then
-                OUT=$(../bin/explanator2.exe -O ${MEASURE} -mode all -fmla tmp/${PREFIX}.mdl -log tmp/${PREFIX}.log \
-                                             -out_mode plain -weights test_weights.ws 2>&1)
-            else
-                OUT=$(../bin/explanator2.exe -O ${MEASURE} -mode all -fmla tmp/${PREFIX}.mdl -log tmp/${PREFIX}.log -out_mode plain \
-                                              2>&1)
-            fi
-        else
-            usage
-        fi
-    fi
-    OUT_GREP=$(grep "Checker output: false\|exception" <<< "${OUT}")
-    N_VERDICTS_EXPLANATOR=$(grep -c "Proof" <<< "${OUT}")
-    N_VERDICTS_VERIMON=$(../../monpoly/monpoly -sig sig/ap.sig -formula tmp/${PREFIX}.mfotl -log tmp/${PREFIX}.log -verified \
-                                               -nonewlastts -verbose 2>&1 | grep -c "false\|true")
-
-    if [[ "${OUT_GREP}" == *"false"* ]]
-    then
-        printf "${PREFIX} ... "
-        printf " !! CHECK FAILED !!\n"
-    fi
-    if [[ "${OUT_GREP}" == *"exception"* ]]
-    then
-        printf "${PREFIX} ... "
-        printf " !! EXCEPTION RAISED !!\n"
-    fi
-    if [[ "${OPERATORS}" = "mtl" ]]
-    then
-        if [[ "${N_VERDICTS_EXPLANATOR}" != "${N_VERDICTS_VERIMON}" ]]
-        then
-            printf "${PREFIX} ... "
-            printf " !! N_VERDICTS_EXPLANATOR = ${N_VERDICTS_EXPLANATOR};"
-            printf " N_VERDICTS_VERIMON = ${N_VERDICTS_VERIMON} !!\n"
-        fi
-    fi
-}
-
 verb () {
-    if [[ "${OPERATORS}" = "mtl" ]]
-    then
-        ./tmp/gen_fmla ${PREFIX} ${SIZE} 10 0 ${SCALE} ${SEED} 16
-    else
-        ./tmp/gen_fmla_full ${PREFIX} ${SIZE} 10 0 ${SCALE} ${SEED} 16
-    fi
+
+    ./tmp/gen_fmla ${PREFIX} ${SIZE} 10 0 ${SCALE} ${SEED} 16
 
     ./tmp/gen_log ${PREFIX} 100 ${ER} ${DELTA} ${SEED} 16
     mv ${PREFIX}.* tmp/
 
-    if [[ "${CHECK_FLAG}" = "check" ]]
+    if [[ "${CHECK_FLAG}" = "verified" ]]
     then
-        if [[ "${WEIGHT_FLAG}" = "weight" ]]
-        then
-            OUT=$(../bin/explanator2.exe -O ${MEASURE} -mode all -fmla tmp/${PREFIX}.mdl -log tmp/${PREFIX}.log \
-                                         -check -out_mode plain -weights test_weights.ws 2>&1)
-        else
-            OUT=$(../bin/explanator2.exe -O ${MEASURE} -mode all -fmla tmp/${PREFIX}.mdl -log tmp/${PREFIX}.log -out_mode plain \
-                                         -check 2>&1)
-        fi
+        OUT=$(../bin/whymon.exe -mode verified -sig sig/ap.sig -formula tmp/${PREFIX}.mfotl -log tmp/${PREFIX}.mlog 2>&1)
     else
-        if [[ "${CHECK_FLAG}" = "no-check" ]]
+        if [[ "${CHECK_FLAG}" = "unverified" ]]
         then
-            if [[ "${WEIGHT_FLAG}" = "weight" ]]
-            then
-                OUT=$(../bin/explanator2.exe -O ${MEASURE} -mode all -fmla tmp/${PREFIX}.mdl -log tmp/${PREFIX}.log \
-                                             -out_mode plain -weights test_weights.ws 2>&1)
-            else
-                OUT=$(../bin/explanator2.exe -O ${MEASURE} -mode all -fmla tmp/${PREFIX}.mdl -log tmp/${PREFIX}.log -out_mode plain \
-                                              2>&1)
-            fi
+            OUT=$(../bin/whymon.exe -mode unverified -sig sig/ap.sig -formula tmp/${PREFIX}.mfotl -log tmp/${PREFIX}.mlog 2>&1)
         else
             usage
         fi
     fi
     OUT_GREP=$(grep "Checker output: false\|exception" <<< "${OUT}")
-    N_VERDICTS_EXPLANATOR=$(grep -c "Proof" <<< "${OUT}")
+    N_VERDICTS_WHYMON=$(grep -c "Explanation" <<< "${OUT}")
     N_VERDICTS_VERIMON=$(../../monpoly/monpoly -sig sig/ap.sig -formula tmp/${PREFIX}.mfotl -log tmp/${PREFIX}.log -verified \
                                                -nonewlastts -verbose 2>&1 | grep -c "false\|true")
 
     printf "${PREFIX} ... "
     if [[ "${OUT_GREP}" == "" ]]
-    then printf "OK"
+    then printf "OK\n"
     fi
     if [[ "${OUT_GREP}" == *"false"* ]]
     then
@@ -140,26 +56,18 @@ verb () {
     then
         printf " !! EXCEPTION RAISED !!"
     fi
-    if [[ "${OPERATORS}" = "mtl" ]]
+    if [[ "${N_VERDICTS_WHYMON}" != "${N_VERDICTS_VERIMON}" ]]
     then
-        if [[ "${N_VERDICTS_EXPLANATOR}" != "${N_VERDICTS_VERIMON}" ]]
-        then
-            printf "${PREFIX} ... "
-            printf " !! N_VERDICTS_EXPLANATOR = ${N_VERDICTS_EXPLANATOR};"
-            printf " N_VERDICTS_VERIMON = ${N_VERDICTS_VERIMON} !!\n"
-        fi
+        printf "${PREFIX} ... "
+        printf " !! N_VERDICTS_WHYMON = ${N_VERDICTS_WHYMON};"
+        printf " N_VERDICTS_VERIMON = ${N_VERDICTS_VERIMON} !!\n"
     fi
     printf "\n"
 }
 
-if [ "$#" -eq 10 ]
+if [ "$#" -eq 6 ]
 then
-    if [[ "${MODE}" == "simp" ]]
-    then simp
-    fi
-    if [[ "${MODE}" == "verb" ]]
-    then time verb
-    fi
+    time verb
 else
     usage
 fi
