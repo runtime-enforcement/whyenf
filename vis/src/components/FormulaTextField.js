84681350c4e4395012254c217b233084181f8a69
Leonardo Lima <leonardo.alfs@proton.me>
2023-10-17 11:15:20 +0200

    Adding virtual keyboard feature

--- vis/src/components/FormulaTextField.js
+++ vis/src/components/FormulaTextField.js
@@ -1,17 +1,39 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import ReactDOM from "react-dom";
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
+import Keyboard from "react-simple-keyboard";
+import "react-simple-keyboard/build/css/index.css";
+import "../keyboard.css";
 
 export default function FormulaTextField ({ formula, setFormState, fixParameters }) {
 
   const [localFormula, setLocalFormula] = useState("");
+  const [isKeyboardOpen, setKeyboardOpen] = useState(false);
+  const keyboard = useRef();
+
+  const openKeyboard = () => {
+    setKeyboardOpen(true);
+  };
+
+  const closeKeyboard = () => {
+    setKeyboardOpen(false);
+  };
+
+  const onChange = input => {
+    setLocalFormula(input);
+  };
 
   const handleChange = (event) => {
-    setLocalFormula(event.target.value);
+    const input = event.target.value;
+    setLocalFormula(input);
+    keyboard.current.setInput(input);
   };
 
   const handleBlur = (event) => {
-    setFormState({ type: 'setFormula', formula: localFormula });
+    setKeyboardOpen(false, setFormState({ type: 'setFormula', formula: localFormula }));
   };
 
   useEffect(() => {
@@ -35,8 +57,19 @@ export default function FormulaTextField ({ formula, setFormState, fixParameters
           value={localFormula}
           onChange={handleChange}
           onBlur={handleBlur}
+          onFocus={openKeyboard}
           disabled={fixParameters}
         />
+        <div className={`keyboardContainer ${!isKeyboardOpen ? "hidden" : ""}`}>
+          <Keyboard
+            keyboardRef={r => (keyboard.current = r)}
+            layoutName={"default"}
+            onChange={onChange}
+            layout={{
+              default: ["⊤ ⊥ = ¬ ∧ ∨ → ↔ ∃ ∀ ● ○ ⧫ ◊ ■ □"]
+            }}
+          />
+        </div>
       </div>
     </Box>
   );
