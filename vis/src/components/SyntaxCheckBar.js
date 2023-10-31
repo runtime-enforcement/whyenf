914df94601cdf038f6584fa4ea3567ce667384e6
Leonardo Lima <leonardo.alfs@proton.me>
2023-10-31 14:37:50 +0100

    Refining syntax check feature

--- vis/src/components/SyntaxCheckBar.js
+++ vis/src/components/SyntaxCheckBar.js
@@ -5,6 +5,7 @@ import Step from '@mui/material/Step';
 import StepLabel from '@mui/material/StepButton';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import CancelIcon from '@mui/icons-material/Cancel';
 
 const steps = ['Signature', 'Formula', 'Trace'];
 
@@ -41,13 +42,45 @@ export default function StatusBar({ checkedInputs }) {
          alignItems="center"
          sx={{ width: '100%', height: '100%' }}>
       <Stepper nonLinear>
-        {steps.map((label, index) => (
-          <Step active expanded key={label} completed={checkedInputs[index]}>
-            <StepLabel color="inherit">
-              {label}
-            </StepLabel>
-          </Step>
-        ))}
+        {steps.map((label, index) => {
+          const labelProps = {};
+          if (checkedInputs[index]) {
+            labelProps.optional = (
+              <Typography variant="caption" color="#388e3c">
+                valid
+              </Typography>
+            );
+          }
+
+          return (
+            <Step active={!checkedInputs[index]} expanded
+                  key={label}
+                  completed={checkedInputs[index]}
+                  sx={{
+                    '& .MuiStepLabel-root .Mui-completed': {
+                      color: 'black', // circle's color
+                    },
+                    '& .MuiStepLabel-label.Mui-completed.MuiStepLabel-alternativeLabel':
+                    {
+                      color: 'black',
+                    },
+                    '& .MuiStepLabel-root .Mui-active': {
+                      color: 'black', // circle's color
+                    },
+                    '& .MuiStepLabel-label.Mui-active.MuiStepLabel-alternativeLabel':
+                    {
+                      color: 'common.white',
+                    },
+                    '& .MuiStepLabel-root .Mui-active .MuiStepIcon-text': {
+                      fill: 'black', // circle's number
+                    },
+                  }}>
+              <StepLabel {...labelProps}>
+                {label}
+              </StepLabel>
+            </Step>
+          );
+        })}
       </Stepper>
     </Box>
   );
