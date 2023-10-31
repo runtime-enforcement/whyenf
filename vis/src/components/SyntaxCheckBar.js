77d983221eea7d8db018f53ecd580e43cfbe32b0
Leonardo Lima <leonardo.alfs@proton.me>
2023-10-31 09:33:29 +0100

    Adding syntax check feature

--- vis/src/components/SyntaxCheckBar.js
+++ vis/src/components/SyntaxCheckBar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Box from '@mui/material/Box';
 import Stepper from '@mui/material/Stepper';
 import Step from '@mui/material/Step';
@@ -6,13 +6,33 @@ import StepLabel from '@mui/material/StepButton';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
-const steps = ['Signature', 'Specification', 'Trace'];
+const steps = ['Signature', 'Formula', 'Trace'];
 
-export default function StatusBar() {
-  const [checked, setChecked] = useState({});
+export default function StatusBar({ checkedInputs }) {
+  const [activeStep, setActiveStep] = React.useState(0);
 
-  const completedSteps = () => {
-    return Object.keys(checked).length;
+  const totalSteps = () => {
+    return steps.length;
+  };
+
+  const checkedSteps = () => {
+    return Object.keys(checkedInputs).length;
+  };
+
+  const isLastStep = () => {
+    return activeStep === totalSteps() - 1;
+  };
+
+  const allStepsChecked = () => {
+    return checkedSteps() === totalSteps();
+  };
+
+  const handleNext = () => {
+    const newActiveStep =
+      isLastStep() && !allStepsChecked()
+        ? steps.findIndex((step, i) => !(i in checkedInputs))
+        : activeStep + 1;
+    setActiveStep(newActiveStep);
   };
 
   return (
@@ -20,9 +40,9 @@ export default function StatusBar() {
          justifyContent="center"
          alignItems="center"
          sx={{ width: '100%', height: '100%' }}>
-      <Stepper activeStep={1}>
+      <Stepper nonLinear>
         {steps.map((label, index) => (
-          <Step key={label} checked={checked[index]}>
+          <Step active expanded key={label} completed={checkedInputs[index]}>
             <StepLabel color="inherit">
               {label}
             </StepLabel>
