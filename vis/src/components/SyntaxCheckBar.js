47e98d50f6de2a6e7c193872f32f0ff8ff0837d4
Leonardo Lima <leonardo.alfs@proton.me>
2023-11-16 23:43:50 +0100

    Removing warnings and unnecessary components

--- vis/src/components/SyntaxCheckBar.js
+++ vis/src/components/SyntaxCheckBar.js
@@ -1,40 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Box from '@mui/material/Box';
 import Stepper from '@mui/material/Stepper';
 import Step from '@mui/material/Step';
 import StepLabel from '@mui/material/StepButton';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import CancelIcon from '@mui/icons-material/Cancel';
 
 const steps = ['Signature', 'Formula', 'Trace'];
 
 export default function StatusBar({ checkedInputs }) {
-  const [activeStep, setActiveStep] = React.useState(0);
-
-  const totalSteps = () => {
-    return steps.length;
-  };
-
-  const checkedSteps = () => {
-    return Object.keys(checkedInputs).length;
-  };
-
-  const isLastStep = () => {
-    return activeStep === totalSteps() - 1;
-  };
-
-  const allStepsChecked = () => {
-    return checkedSteps() === totalSteps();
-  };
-
-  const handleNext = () => {
-    const newActiveStep =
-      isLastStep() && !allStepsChecked()
-        ? steps.findIndex((step, i) => !(i in checkedInputs))
-        : activeStep + 1;
-    setActiveStep(newActiveStep);
-  };
 
   return (
     <Box display="flex"
