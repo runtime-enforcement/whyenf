19189a9cda98e1895810a2b23c442df7631e5ecc
Leonardo Lima <leonardo.alfs@proton.me>
2023-12-02 16:30:57 +0100

    Fixing signature

--- src/enforcer.mli
+++ src/enforcer.mli
@@ -1,1 +1,11 @@
-val exec : Formula.t -> unit
+(*******************************************************************)
+(*     This is part of WhyMon, and it is distributed under the     *)
+(*     terms of the GNU Lesser General Public License version 3    *)
+(*           (see file LICENSE for more details)                   *)
+(*                                                                 *)
+(*  Copyright 2023:                                                *)
+(*  François Hublet (ETH Zurich)                                   *)
+(*  Leonardo Lima (UCPH)                                           *)
+(*******************************************************************)
+
+val exec : string -> Formula.t -> in_channel -> unit
