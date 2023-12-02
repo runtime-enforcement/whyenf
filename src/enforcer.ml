a471f0086b6bd031c8a4573693e78d2d9db0c0fc
Leonardo Lima <leonardo.alfs@proton.me>
2023-12-02 16:17:19 +0100

    Starting with enf functions

--- src/enforcer.ml
+++ src/enforcer.ml
@@ -13,44 +13,93 @@ open Stdio
 open Etc
 open Monitor
 
+type ts_to_formula = timestamp -> Formula.t
+
+type polarity = POS | NEG
+
+type commitments = (ts_to_formula * polarity) list
+
 module EState = struct
 
   type t = { mf: MFormula.t
            ; tp_cur: timepoint
            ; tp_out: timepoint
-           ; ts_waiting: timestamp Queue.t }
+           ; ts_waiting: timestamp Queue.t
+           ; tsdbs: (timestamp * Db.t) Queue.t
+           ; commitments: commitments }
 
   let init mf = { mf = mf
                 ; tp_cur = 0
                 ; tp_out = -1
-                ; ts_waiting = Queue.create () }
+                ; ts_waiting = Queue.create ()
+                ; tsdbs = Queue.create ()
+                ; commitments = [] }
 
   let to_string { mf
                 ; tp_cur
                 ; tp_out
-                ; ts_waiting } =
+                ; ts_waiting
+                ; tsdbs
+                ; commitments } =
     "\nEState: \n\n" ^
       Printf.sprintf "mf = %s\n" (MFormula.to_string mf) ^
         Printf.sprintf "tp_cur = %d\n" tp_cur ^
           Printf.sprintf "tp_out = %d\n" tp_out ^
             "\nts_waiting = [" ^ (String.concat ~sep:", "
-                                    (Queue.to_list (Queue.map ts_waiting ~f:Int.to_string))) ^ "]\n"
+                                    (Queue.to_list (Queue.map ts_waiting ~f:Int.to_string))) ^ "]\n" ^
+              "\ntsdbs = [" ^ (String.concat ~sep:", "
+                                 (Queue.to_list (Queue.map tsdbs ~f:(fun (ts, db) ->
+                                                     Printf.sprintf "(%d):\n %s\n" ts (Db.to_string db))))) ^ "]\n"
 
 end
 
-type ts_to_formula = timestamp -> Formula.t
+(* let enfsat (f: Formula.t) ts tp is_tp_last coms tsdbs = match f with *)
+(*   | TT -> ([], [], []) *)
+(*   | Predicate (r, trms) -> ([], [], []) *)
+(*   | Neg f -> ([], [], []) *)
+(*   | And (side, f1, f2) -> ([], [], []) *)
+(*   | Or (side, f1, f2) -> ([], [], []) *)
+(*   | Imp (side, f1, f2) -> ([], [], []) *)
+(*   | Iff (side1, side2, f1, f2) -> ([], [], []) *)
+(*   | Exists (x, f) -> ([], [], []) *)
+(*   | Forall (x, f) -> ([], [], []) *)
+(*   | Next (i, f) -> ([], [], []) *)
+(*   | Once (i, f) -> ([], [], []) *)
+(*   | Eventually (i, f) -> ([], [], []) *)
+(*   | Historically (i, f) -> ([], [], []) *)
+(*   | Always (i, f) -> ([], [], []) *)
+(*   | Since (side, i, f1, f2) -> ([], [], []) *)
+(*   | Until (side, i, f1, f2) -> ([], [], []) *)
+(*   | _ -> raise (Invalid_argument ("function enfsat is not defined for " ^ Formula.op_to_string f)) *)
 
-type polarity = POS | NEG
 
-type future_obligations = ts_to_formula * polarity
+(* let enfvio (f: Formula.t) ts tp is_tp_last coms tsdbs = match f with *)
+(*   | FF -> ([], [], []) *)
+(*   | Predicate (r, trms) -> ([], [], []) *)
+(*   | Neg f -> ([], [], []) *)
+(*   | And (side, f1, f2) -> ([], [], []) *)
+(*   | Or (side, f1, f2) -> ([], [], []) *)
+(*   | Imp (side, f1, f2) -> ([], [], []) *)
+(*   | Iff (side1, side2, f1, f2) -> ([], [], []) *)
+(*   | Exists (x, f) -> ([], [], []) *)
+(*   | Forall (x, f) -> ([], [], []) *)
+(*   | Next (i, f) -> ([], [], []) *)
+(*   | Once (i, f) -> ([], [], []) *)
+(*   | Eventually (i, f) -> ([], [], []) *)
+(*   | Historically (i, f) -> ([], [], []) *)
+(*   | Always (i, f) -> ([], [], []) *)
+(*   | Since (side, i, f1, f2) -> ([], [], []) *)
+(*   | Until (side, i, f1, f2) -> ([], [], []) *)
+(*   | _ -> raise (Invalid_argument ("function enfvio is not defined for " ^ Formula.op_to_string f)) *)
 
-(* let eeval = () *)
+(* let enf ts tp is_tp_last coms tsdbs = *)
 
 (* let estep vars ts db (es: EState.t) = *)
-(*   let (expls, mf') = eeval vars ts es.tp_cur db es.mf in *)
+(*   let (expls, mf') = enf vars ts es.tp_cur db es.mf in *)
 (*   Queue.enqueue es.ts_waiting ts; *)
 (*   let tstps = List.zip_exn (List.take (Queue.to_list es.ts_waiting) (List.length expls)) *)
 (*                 (List.range es.tp_cur (es.tp_cur + List.length expls)) in *)
+(*   Queue.enqueue es.tsdbs (ts, db); *)
 (*   (List.zip_exn tstps expls, *)
 (*    { es with *)
 (*      mf = mf' *)
