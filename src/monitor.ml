runtime-enforcement/whyenf

commit 9b30946a6b3f07a2cecc36b9fcd5b00b4286690c

Fixing ts/tp in CLI output

src/monitor.ml
@@ -1533,7 +1533,7 @@ module MState = struct
 
   let init mf = { mf = mf
                 ; tp_cur = 0
-                ; tp_out = -1
+                ; tp_out = 0
                 ; ts_waiting = Queue.create ()
                 ; tsdbs = Queue.create ()
                 ; tpts = Hashtbl.create (module Int) }
@@ -1563,7 +1563,7 @@ let mstep mode vars ts db (ms: MState.t) =
   let (expls, mf') = meval vars ts ms.tp_cur db ms.mf in
   Queue.enqueue ms.ts_waiting ts;
   let tstps = List.zip_exn (List.take (Queue.to_list ms.ts_waiting) (List.length expls))
-                (List.range ms.tp_cur (ms.tp_cur + List.length expls)) in
+                (List.range ms.tp_out (ms.tp_out + List.length expls)) in
   let tsdbs = match mode with
     | Out.Plain.UNVERIFIED -> ms.tsdbs
     | _ -> Queue.enqueue ms.tsdbs (ts, db); ms.tsdbs in
@@ -1571,6 +1571,7 @@ let mstep mode vars ts db (ms: MState.t) =
    { ms with
      mf = mf'
    ; tp_cur = ms.tp_cur + 1
+   ; tp_out = ms.tp_out + (List.length expls)
    ; ts_waiting = queue_drop ms.ts_waiting (List.length expls)
    ; tsdbs = tsdbs })
 
@@ -1581,17 +1582,17 @@ let exec mode measure f inc =
     | Some (more, pb) ->
        (let (tstp_expls, ms') = mstep mode (Set.elements (Formula.fv f)) pb.ts pb.db ms in
         (match mode with
-         | Out.Plain.UNVERIFIED -> Out.Plain.expls pb.ts tstp_expls None None None mode
-         | Out.Plain.LATEX -> Out.Plain.expls pb.ts tstp_expls None None (Some(f)) mode
-         | Out.Plain.LIGHT -> Out.Plain.expls pb.ts tstp_expls None None None mode
+         | Out.Plain.UNVERIFIED -> Out.Plain.expls tstp_expls None None None mode
+         | Out.Plain.LATEX -> Out.Plain.expls tstp_expls None None (Some(f)) mode
+         | Out.Plain.LIGHT -> Out.Plain.expls tstp_expls None None None mode
          | Out.Plain.DEBUGVIS -> raise (Failure "function exec is undefined for the mode debugvis")
          | Out.Plain.VERIFIED ->
             let c = Checker_interface.check (Queue.to_list ms'.tsdbs) f (List.map tstp_expls ~f:snd) in
-            Out.Plain.expls pb.ts tstp_expls (Some(c)) None None mode
+            Out.Plain.expls tstp_expls (Some(c)) None None mode
          | Out.Plain.DEBUG ->
             let c = Checker_interface.check (Queue.to_list ms'.tsdbs) f (List.map tstp_expls ~f:snd) in
             let paths = Checker_interface.false_paths (Queue.to_list ms'.tsdbs) f (List.map tstp_expls ~f:snd) in
-            Out.Plain.expls pb.ts tstp_expls (Some(c)) (Some(paths)) None mode);
+            Out.Plain.expls tstp_expls (Some(c)) (Some(paths)) None mode);
         if more then step (Some(pb)) ms') in
   let mf = init f in
   let ms = MState.init mf in

src/out.ml
@@ -48,16 +48,16 @@ module Plain = struct
         );
     | Info s -> Stdio.printf "\nInfo: %s\n\n" s
 
-  let expls ts tstp_expls checker_es_opt paths_opt f_opt = function
-    | UNVERIFIED -> List.iter tstp_expls ~f:(fun ((_, tp), e) -> expl (Explanation ((ts, tp), e)))
+  let expls tstp_expls checker_es_opt paths_opt f_opt = function
+    | UNVERIFIED -> List.iter tstp_expls ~f:(fun ((ts, tp), e) -> expl (Explanation ((ts, tp), e)))
     | VERIFIED -> List.iter2_exn tstp_expls (Option.value_exn checker_es_opt)
-                    ~f:(fun ((_, tp), e) (b, _, _) -> expl (ExplanationCheck ((ts, tp), e, b)))
-    | LATEX -> List.iter tstp_expls ~f:(fun ((_, tp), e) ->
+                    ~f:(fun ((ts, tp), e) (b, _, _) -> expl (ExplanationCheck ((ts, tp), e, b)))
+    | LATEX -> List.iter tstp_expls ~f:(fun ((ts, tp), e) ->
                    expl (ExplanationLatex ((ts, tp), e, Option.value_exn f_opt)))
-    | LIGHT -> List.iter tstp_expls ~f:(fun ((_, tp), e) -> if Expl.is_violated e then expl (ExplanationLight ((ts, tp), e)))
+    | LIGHT -> List.iter tstp_expls ~f:(fun ((ts, tp), e) -> if Expl.is_violated e then expl (ExplanationLight ((ts, tp), e)))
     | DEBUG -> List.iter2_exn (List.zip_exn tstp_expls (Option.value_exn checker_es_opt))
                  (Option.value_exn paths_opt)
-                 ~f:(fun (((_, tp), e), (b, checker_e, trace)) path_opt ->
+                 ~f:(fun (((ts, tp), e), (b, checker_e, trace)) path_opt ->
                    expl (ExplanationCheckDebug ((ts, tp), e, b, checker_e, trace, path_opt)))
     | DEBUGVIS -> raise (Failure "this function is undefined for the mode debugvis")
 

src/out.mli
@@ -15,7 +15,7 @@ module Plain : sig
 
   type mode = UNVERIFIED | VERIFIED | LATEX | LIGHT | DEBUG | DEBUGVIS
 
-  val expls: timepoint -> ((timestamp * timepoint) * Expl.t) list ->
+  val expls: ((timestamp * timepoint) * Expl.t) list ->
              (bool * Checker_pdt.t * Checker_trace.t) list option ->
              ((Domain.t, Domain.comparator_witness) Setc.t list list option list) option ->
              Formula.t option -> mode -> unit

commit 57654f6a7742bd84afcff4391515b1ab6fc1db53

Fixing visualization bug introduced in 9b30946

src/monitor.ml
@@ -1533,7 +1533,7 @@ module MState = struct
 
   let init mf = { mf = mf
                 ; tp_cur = 0
-                ; tp_out = 0
+                ; tp_out = -1
                 ; ts_waiting = Queue.create ()
                 ; tsdbs = Queue.create ()
                 ; tpts = Hashtbl.create (module Int) }
@@ -1563,7 +1563,7 @@ let mstep mode vars ts db (ms: MState.t) =
   let (expls, mf') = meval vars ts ms.tp_cur db ms.mf in
   Queue.enqueue ms.ts_waiting ts;
   let tstps = List.zip_exn (List.take (Queue.to_list ms.ts_waiting) (List.length expls))
-                (List.range ms.tp_out (ms.tp_out + List.length expls)) in
+                (List.range ms.tp_cur (ms.tp_cur + List.length expls)) in
   let tsdbs = match mode with
     | Out.Plain.UNVERIFIED -> ms.tsdbs
     | _ -> Queue.enqueue ms.tsdbs (ts, db); ms.tsdbs in
@@ -1571,7 +1571,6 @@ let mstep mode vars ts db (ms: MState.t) =
    { ms with
      mf = mf'
    ; tp_cur = ms.tp_cur + 1
-   ; tp_out = ms.tp_out + (List.length expls)
    ; ts_waiting = queue_drop ms.ts_waiting (List.length expls)
    ; tsdbs = tsdbs })
 
@@ -1613,8 +1612,7 @@ let exec_vis (obj_opt: MState.t option) f log =
                              ~f:(fun acc ((ts, tp), _) -> Hashtbl.add_exn ms.tpts ~key:(acc + 1) ~data:ts; acc + 1) in
              let json_expls = Out.Json.expls ms.tpts f (List.map tstps_expls ~f:snd) in
              let json_db = Out.Json.db pb.ts ms.tp_cur pb.db f in
-             Some (None, (json_expls, [json_db]),
-                   { ms' with tp_out = tp_out' })
+             Some (None, (json_expls, [json_db]), { ms' with tp_out = tp_out' })
            else raise (Failure "trace is not monotonic")))
     with Failure msg -> Some (Some(msg), ([], []), ms) in
   let ms = match obj_opt with