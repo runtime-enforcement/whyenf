117530c4cda5fe0f051f23e7b4ac62e5adf0f4ab
fhublet <fhublet@ethz.ch>
2025-01-13 15:22:53 +0100

    Minor

--- eval/enforcement/evaluate_gdpr.py
+++ eval/enforcement/evaluate_gdpr.py
@@ -5,7 +5,7 @@ run_experiments(
     benchmark     = 'gdpr',
     exe           = './monpoly.exe',
     accelerations = [1e5, 2e5, 4e5, 8e5, 1.6e6, 3.2e6, 6.4e6, 1.28e7, 2.56e7, 5.12e7],
-    n             = 1,#10,
+    n             = 5,#10,
     time_unit     = 24 * 3600,
 )
 
