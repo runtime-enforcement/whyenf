289cb247657b28972fcc8c4155ee1ce85ec0fe13
Leonardo Lima <leonardo.alfs@proton.me>
2025-04-14 14:28:08 +0200

    Replacing LIFEBOAT -> EnfGuard

--- eval/enforcement/evaluate_agg.py
+++ eval/enforcement/evaluate_agg.py
@@ -11,12 +11,11 @@ run_experiments(
 )
 
 run_experiments(
-    option        = 'lifeboat',
+    option        = 'enfguard',
     benchmark     = 'agg',
-    exe           = './lifeboat.exe',
+    exe           = './enfguard.exe',
     accelerations = [1, 2, 4, 8, 16, 32, 64, 128, 256],
     n             = 1,#10,
     time_unit     = 1,
     only_graph    = True,
 )
-
--- eval/enforcement/evaluate_nokia.py
+++ eval/enforcement/evaluate_nokia.py
@@ -12,9 +12,9 @@ run_experiments(
 )
 
 run_experiments(
-    option        = 'lifeboat',
+    option        = 'enfguard',
     benchmark     = 'nokia',
-    exe           = './lifeboat.exe',
+    exe           = './enfguard.exe',
     accelerations = [32, 64, 128, 256],#[1, 2, 4, 8, 16, 32, 64, 128, 256],
     n             = 1,#10,
     time_unit     = 1,
@@ -40,4 +40,3 @@ run_experiments(
     time_unit     = 1,
     only_graph    = True,
 )
-
